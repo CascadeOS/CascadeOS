ec5faf649b08cb5e31527f3ad9e109a92dd674e9
Lee Cannon <leecannon@leecannon.xyz>
2023-05-20 20:13:13 +0100

    kernel/panic: implement a system to switch between different panic implementations

--- kernel/kernel.zig
+++ kernel/kernel.zig
@@ -8,3 +8,43 @@ pub const info = @import("info.zig");
 comptime {
     _ = arch.current;
 }
+
+/// The signature of a panic implementation function.
+pub const PanicFunction = *const fn (
+    msg: []const u8,
+    stack_trace: ?*const std.builtin.StackTrace,
+    ret_addr: ?usize,
+) noreturn;
+
+/// Set the function pointer to the implementation of panic.
+pub fn setPanicFunction(panic_fn: PanicFunction) void {
+    panic_impl = panic_fn;
+}
+
+/// A function pointer to the current implementation of panic.
+/// This will change from "`noOpPanic` -> setup panic -> normal panic" during system setup.
+var panic_impl: PanicFunction = noOpPanic;
+
+/// A no-op panic used before any infrastructure is ready.
+fn noOpPanic(
+    msg: []const u8,
+    stack_trace: ?*const std.builtin.StackTrace,
+    ret_addr: ?usize,
+) noreturn {
+    _ = ret_addr;
+    _ = stack_trace;
+    _ = msg;
+    while (true) {
+        arch.current.disableInterruptsAndHalt();
+    }
+}
+
+/// Entry point from the Zig language upon a panic.
+pub fn panic(
+    msg: []const u8,
+    stack_trace: ?*const std.builtin.StackTrace,
+    ret_addr: ?usize,
+) noreturn {
+    @setCold(true);
+    panic_impl(msg, stack_trace, ret_addr);
+}
