02682cd272c876340f80af6cecc67fe331834cfe
Lee Cannon <leecannon@leecannon.xyz>
2023-05-20 20:11:57 +0100

    kernel/x86_64: implement `disableInterruptsAndHalt`

--- kernel/arch/x86_64/x86_64.zig
+++ kernel/arch/x86_64/x86_64.zig
@@ -5,3 +5,10 @@ const std = @import("std");
 export fn _start() callconv(.Naked) void {
     while (true) {}
 }
+
+/// Disable interrupts and put the CPU to sleep.
+pub fn disableInterruptsAndHalt() noreturn {
+    while (true) {
+        asm volatile ("cli; hlt");
+    }
+}
