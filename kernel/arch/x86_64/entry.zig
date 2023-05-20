3d0b4a24b3cbe034bf067eda44b8000cf04223a6
Lee Cannon <leecannon@leecannon.xyz>
2023-05-20 22:03:41 +0100

    kernel/x86_64/entry: call into single setup function after control is given from bootloader

--- kernel/arch/x86_64/entry.zig
+++ kernel/arch/x86_64/entry.zig
@@ -5,14 +5,27 @@ const kernel = @import("root");
 
 const limine = kernel.spec.limine;
 
+fn setup(bootloader: Bootloader) noreturn {
+    _ = bootloader;
+
+    @panic("UNIMPLEMENTED"); // TODO: implement initial system setup
+}
+
+const Bootloader = enum {
+    unknown,
+    limine,
+};
+
 /// Generic entry point.
 export fn _start() callconv(.Naked) noreturn {
-    while (true) {}
+    @call(.never_inline, setup, .{.unknown});
+    @panic("setup returned");
 }
 
 /// Entry point for limine.
 export fn _limine_start() callconv(.Naked) noreturn {
-    while (true) {}
+    @call(.never_inline, setup, .{.limine});
+    @panic("setup returned");
 }
 
 export var limine_entry_point_request = limine.EntryPoint{
