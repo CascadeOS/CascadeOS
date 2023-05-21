8fcfdd4a1139f6c855c546cecfbb627195719392
Lee Cannon <leecannon@leecannon.xyz>
2023-05-21 15:36:09 +0100

    kernel/arch/entry: simplify entry points as only limine is supported currently

--- kernel/arch/aarch64/entry.zig
+++ kernel/arch/aarch64/entry.zig
@@ -5,29 +5,12 @@ const kernel = @import("root");
 
 const limine = kernel.spec.limine;
 
-fn setup(bootloader: Bootloader) noreturn {
-    _ = bootloader;
-
+fn setup() void {
     @panic("UNIMPLEMENTED"); // TODO: implement initial system setup
 }
 
-const Bootloader = enum {
-    unknown,
-    limine,
-};
-
-/// Generic entry point.
+/// Entry point.
 export fn _start() callconv(.Naked) noreturn {
-    @call(.never_inline, setup, .{.unknown});
+    @call(.never_inline, setup, .{});
     @panic("setup returned");
 }
-
-/// Entry point for limine.
-export fn _limine_start() callconv(.Naked) noreturn {
-    @call(.never_inline, setup, .{.limine});
-    @panic("setup returned");
-}
-
-export var limine_entry_point_request = limine.EntryPoint{
-    .entry = _limine_start,
-};
