9ec4aacab3b00066b4076422683fedc6d141d844
Lee Cannon <leecannon@leecannon.xyz>
2023-05-23 21:32:31 +0100

    minor refactor

--- kernel/arch/aarch64/aarch64.zig
+++ kernel/arch/aarch64/aarch64.zig
@@ -3,11 +3,11 @@
 const std = @import("std");
 const kernel = @import("root");
 
-pub const instructions = @import("instructions.zig");
-pub const setup = @import("setup.zig");
-pub const Uart = @import("Uart.zig");
-
 comptime {
     // make sure the entry points are referenced
     _ = setup;
 }
+
+pub const instructions = @import("instructions.zig");
+pub const setup = @import("setup.zig");
+pub const Uart = @import("Uart.zig");
--- kernel/arch/x86_64/x86_64.zig
+++ kernel/arch/x86_64/x86_64.zig
@@ -3,11 +3,11 @@
 const std = @import("std");
 const kernel = @import("root");
 
-pub const instructions = @import("instructions.zig");
-pub const serial = @import("serial.zig");
-pub const setup = @import("setup.zig");
-
 comptime {
     // make sure the entry points are referenced
     _ = setup;
 }
+
+pub const instructions = @import("instructions.zig");
+pub const serial = @import("serial.zig");
+pub const setup = @import("setup.zig");
