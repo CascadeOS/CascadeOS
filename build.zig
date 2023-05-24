d12821600fd7e1cac1879379c62b3fd2003e0f2a
Lee Cannon <leecannon@leecannon.xyz>
2023-05-24 22:43:34 +0100

    build: add library system

--- build.zig
+++ build.zig
@@ -11,6 +11,8 @@ pub fn build(b: *std.Build) !void {
 
     const options = try Options.get(b);
 
+    const libraries = try createLibraries(b, step_collection, options.optimize);
+    _ = libraries;
     const kernels = try createKernels(b, step_collection, options);
     const images = try createImageSteps(b, kernels);
     try createQemuSteps(b, images, options);
@@ -28,6 +30,17 @@ pub const CircuitTarget = union(Arch) {
     pub const Arch = enum {
         aarch64,
         x86_64,
+
+        pub fn getTestCrossTarget(self: Arch) std.zig.CrossTarget {
+            switch (self) {
+                .aarch64 => return std.zig.CrossTarget{
+                    .cpu_arch = .aarch64,
+                },
+                .x86_64 => return std.zig.CrossTarget{
+                    .cpu_arch = .x86_64,
+                },
+            }
+        }
     };
 
     pub const AArch64Board = enum {
@@ -518,12 +531,19 @@ const Kernel = struct {
 const StepCollection = struct {
     main_test_step: *Step,
     test_steps: std.AutoHashMapUnmanaged(CircuitTarget, *Step),
+    libraries_test_step: *Step,
 
     pub fn create(b: *std.Build) !StepCollection {
         const main_test_step = b.step(
             "test",
             "Run all the tests (also builds all code even if they don't have tests)",
         );
+
+        const libraries_test_step = b.step(
+            "test_libraries",
+            "Run all the library tests",
+        );
+        main_test_step.dependOn(libraries_test_step);
 
         var test_steps = std.AutoHashMapUnmanaged(CircuitTarget, *Step){};
         errdefer test_steps.deinit(b.allocator);
@@ -551,6 +571,7 @@ const StepCollection = struct {
         return StepCollection{
             .main_test_step = main_test_step,
             .test_steps = test_steps,
+            .libraries_test_step = libraries_test_step,
         };
     }
 };
@@ -891,6 +912,175 @@ pub fn fileExists(path: []const u8) bool {
     return true;
 }
 
+const Libraries = std.StringArrayHashMapUnmanaged(*Library);
+
+fn createLibraries(b: *std.Build, step_collection: StepCollection, optimize: std.builtin.OptimizeMode) !Libraries {
+    const library_list: []const LibraryDescription = @import("libraries/listing.zig").libraries;
+
+    var libraries: Libraries = .{};
+    try libraries.ensureTotalCapacity(b.allocator, library_list.len);
+
+    // The library descriptions still left to resolve
+    var libraries_todo = try std.ArrayListUnmanaged(LibraryDescription).initCapacity(b.allocator, library_list.len);
+
+    // Fill the todo list with all the libraries
+    libraries_todo.appendSliceAssumeCapacity(library_list);
+
+    while (libraries_todo.items.len != 0) {
+        var resolved_any_this_loop = false;
+
+        var i: usize = 0;
+        while (i < libraries_todo.items.len) {
+            const description: LibraryDescription = libraries_todo.items[i];
+
+            if (try resolveLibrary(b, description, libraries, step_collection, optimize)) |library| {
+                libraries.putAssumeCapacityNoClobber(description.name, library);
+
+                resolved_any_this_loop = true;
+                _ = libraries_todo.swapRemove(i);
+            } else {
+                i += 1;
+            }
+        }
+
+        if (!resolved_any_this_loop) {
+            @panic("STUCK IN A LOOP"); // TODO: Report this better
+        }
+    }
+
+    return libraries;
+}
+
+fn resolveLibrary(
+    b: *std.Build,
+    description: LibraryDescription,
+    libraries: Libraries,
+    step_collection: StepCollection,
+    optimize: std.builtin.OptimizeMode,
+) !?*Library {
+    const library_dependencies = blk: {
+        var library_dependencies = try std.ArrayList(*Library).initCapacity(b.allocator, description.dependencies.len);
+        defer library_dependencies.deinit();
+
+        for (description.dependencies) |dep| {
+            if (libraries.get(dep)) |dep_library| {
+                library_dependencies.appendAssumeCapacity(dep_library);
+            } else {
+                return null;
+            }
+        }
+
+        break :blk try library_dependencies.toOwnedSlice();
+    };
+
+    const root_file = try std.fmt.allocPrint(b.allocator, "{s}.zig", .{description.name});
+
+    const root_path = pathJoinFromRoot(b, &.{
+        "libraries",
+        description.name,
+        root_file,
+    });
+
+    const file_source: std.Build.FileSource = .{ .path = root_path };
+
+    const module = blk: {
+        const module = b.createModule(.{
+            .source_file = file_source,
+        });
+
+        for (library_dependencies) |library| {
+            try module.dependencies.put(library.name, library.module);
+        }
+
+        break :blk module;
+    };
+
+    if (description.supported_architectures) |supported_architectures| {
+        for (supported_architectures) |arch| {
+            const test_exe = b.addTest(.{
+                .name = description.name,
+                .root_source_file = file_source,
+                .optimize = optimize,
+                .target = arch.getTestCrossTarget(),
+            });
+
+            for (library_dependencies) |library| {
+                test_exe.addModule(library.name, library.module);
+            }
+
+            const run_test_exe = b.addRunArtifact(test_exe);
+            run_test_exe.skip_foreign_checks = true;
+
+            const run_step_name = try std.fmt.allocPrint(
+                b.allocator,
+                "test_{s}_{s}",
+                .{ description.name, @tagName(arch) },
+            );
+            const run_step_description = try std.fmt.allocPrint(
+                b.allocator,
+                "Run the tests for {s}_{s}",
+                .{ description.name, @tagName(arch) },
+            );
+
+            const run_step = b.step(run_step_name, run_step_description);
+            run_step.dependOn(&run_test_exe.step);
+
+            step_collection.libraries_test_step.dependOn(run_step);
+        }
+    } else {
+        // Don't provide a target so that the tests are built for the host
+        const test_exe = b.addTest(.{
+            .name = description.name,
+            .root_source_file = file_source,
+            .optimize = optimize,
+        });
+
+        for (library_dependencies) |library| {
+            test_exe.addModule(library.name, library.module);
+        }
+
+        const run_test_exe = b.addRunArtifact(test_exe);
+
+        const run_step_name = try std.fmt.allocPrint(
+            b.allocator,
+            "test_{s}",
+            .{description.name},
+        );
+        const run_step_description = try std.fmt.allocPrint(
+            b.allocator,
+            "Run the tests for {s}",
+            .{description.name},
+        );
+
+        const run_step = b.step(run_step_name, run_step_description);
+        run_step.dependOn(&run_test_exe.step);
+
+        step_collection.libraries_test_step.dependOn(run_step);
+    }
+
+    var library = try b.allocator.create(Library);
+
+    library.* = .{
+        .name = description.name,
+        .root_file = file_source,
+        .module = module,
+        .dependencies = library_dependencies,
+    };
+
+    return library;
+}
+
+const Library = struct {
+    name: []const u8,
+    root_file: std.Build.FileSource,
+    dependencies: []const *Library,
+    module: *std.Build.Module,
+
+    pub fn getRootFilePath(library: *const Library, b: *std.Build) []const u8 {
+        return library.root_file.getPath(b);
+    }
+};
+
 pub const LibraryDescription = struct {
     /// The name of the library:
     ///   - used as the name of the module provided `@import("{name}");`
@@ -899,4 +1089,8 @@ pub const LibraryDescription = struct {
     name: []const u8,
 
     dependencies: []const []const u8 = &.{},
+
+    /// The list of architectures supported by the library.
+    /// `null` means architecture-independent.
+    supported_architectures: ?[]const CircuitTarget.Arch = null,
 };
---
+++ libraries/listing.zig
@@ -0,0 +1,4 @@
+const build = @import("../build.zig");
+const LibraryDescription = build.LibraryDescription;
+
+pub const libraries: []const LibraryDescription = &.{};
