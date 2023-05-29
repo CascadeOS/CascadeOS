532d30fadfa1eb3f0049e96d478e99ae7ed0609f
Lee Cannon <leecannon@leecannon.xyz>
2023-05-29 16:42:22 +0100

    kernel/addr: use a generic function to deduplicate the addr implementation

--- kernel/addr.zig
+++ kernel/addr.zig
@@ -4,167 +4,119 @@ const std = @import("std");
 const core = @import("core");
 const kernel = @import("kernel");
 
-pub const PhysAddr = extern struct {
-    value: usize,
-
-    pub const zero: PhysAddr = .{ .value = 0 };
-
-    pub inline fn fromInt(value: usize) PhysAddr {
-        // TODO: check that the address is valid (cannoical)
-        return .{ .value = value };
-    }
-
-    pub inline fn toKernelVirtual(self: PhysAddr) VirtAddr {
-        return .{ .value = self.value + kernel.info.hhdm.addr.value };
-    }
-
-    pub inline fn isAligned(self: PhysAddr, alignment: core.Size) bool {
-        return std.mem.isAligned(self.value, alignment.bytes);
-    }
-
-    pub inline fn moveForward(self: PhysAddr, size: core.Size) PhysAddr {
-        return .{ .value = self.value + size.bytes };
-    }
-
-    pub inline fn moveForwardInPlace(self: *PhysAddr, size: core.Size) void {
-        self.value += size.bytes;
-    }
-
-    pub inline fn moveBackward(self: PhysAddr, size: core.Size) PhysAddr {
-        return .{ .value = self.value - size.bytes };
-    }
-
-    pub inline fn moveBackwardInPlace(self: *PhysAddr, size: core.Size) void {
-        self.value -= size.bytes;
-    }
-
-    pub inline fn lessThan(self: PhysAddr, other: PhysAddr) bool {
-        return self.value < other.value;
-    }
-
-    pub inline fn lessThanOrEqual(self: PhysAddr, other: PhysAddr) bool {
-        return self.value <= other.value;
-    }
-
-    pub inline fn greaterThan(self: PhysAddr, other: PhysAddr) bool {
-        return self.value > other.value;
-    }
-
-    pub inline fn greaterThanOrEqual(self: PhysAddr, other: PhysAddr) bool {
-        return self.value >= other.value;
-    }
-
-    pub inline fn equal(self: PhysAddr, other: PhysAddr) bool {
-        return self.value == other.value;
-    }
-
-    pub fn format(
-        self: PhysAddr,
-        comptime fmt: []const u8,
-        options: std.fmt.FormatOptions,
-        writer: anytype,
-    ) !void {
-        _ = fmt;
-        _ = options;
-
-        try writer.writeAll("PhysAddr{ 0x");
-        try std.fmt.formatInt(self.value, 16, .lower, .{ .width = 16, .fill = '0' }, writer);
-        try writer.writeAll(" }");
-    }
-
-    comptime {
-        std.debug.assert(@sizeOf(PhysAddr) == @sizeOf(usize));
-        std.debug.assert(@bitSizeOf(PhysAddr) == @bitSizeOf(usize));
-    }
+const AddrType = enum {
+    phys,
+    virt,
 };
 
-pub const VirtAddr = extern struct {
-    value: usize,
+pub const PhysAddr = Addr(.phys);
+pub const VirtAddr = Addr(.virt);
 
-    pub const zero: VirtAddr = .{ .value = 0 };
+fn Addr(comptime addr_type: AddrType) type {
+    return extern struct {
+        value: usize,
 
-    pub inline fn fromInt(value: usize) VirtAddr {
-        // TODO: check that the address is valid (cannoical)
-        return .{ .value = value };
-    }
+        const Self = @This();
 
-    pub inline fn fromPtr(ptr: *const anyopaque) VirtAddr {
-        return fromInt(@ptrToInt(ptr));
-    }
+        pub const zero: Self = .{ .value = 0 };
 
-    pub inline fn toPtr(self: VirtAddr, comptime PtrT: type) PtrT {
-        return @intToPtr(PtrT, self.value);
-    }
-
-    pub fn toPhysicalFromKernelVirtual(self: VirtAddr) !PhysAddr {
-        if (kernel.info.hhdm.contains(self)) {
-            return .{ .value = self.value - kernel.info.hhdm.addr.value };
+        pub inline fn fromInt(value: usize) Self {
+            // TODO: check that the address is valid (cannoical)
+            return .{ .value = value };
         }
-        if (kernel.info.non_cached_hhdm.contains(self)) {
-            return .{ .value = self.value - kernel.info.non_cached_hhdm.addr.value };
+
+        pub inline fn toKernelVirtual(self: PhysAddr) VirtAddr {
+            return .{ .value = self.value + kernel.info.hhdm.addr.value };
         }
-        return error.AddressNotInAnyHHDM;
-    }
 
-    pub inline fn isAligned(self: VirtAddr, alignment: core.Size) bool {
-        return std.mem.isAligned(self.value, alignment.bytes);
-    }
+        pub inline fn fromPtr(ptr: *const anyopaque) VirtAddr {
+            return fromInt(@ptrToInt(ptr));
+        }
 
-    pub inline fn moveForward(self: VirtAddr, size: core.Size) VirtAddr {
-        return .{ .value = self.value + size.bytes };
-    }
+        pub inline fn toPtr(self: VirtAddr, comptime PtrT: type) PtrT {
+            return @intToPtr(PtrT, self.value);
+        }
 
-    pub inline fn moveForwardInPlace(self: *VirtAddr, size: core.Size) void {
-        self.value += size.bytes;
-    }
+        pub fn toPhysicalFromKernelVirtual(self: VirtAddr) !PhysAddr {
+            if (kernel.info.hhdm.contains(self)) {
+                return .{ .value = self.value - kernel.info.hhdm.addr.value };
+            }
+            if (kernel.info.non_cached_hhdm.contains(self)) {
+                return .{ .value = self.value - kernel.info.non_cached_hhdm.addr.value };
+            }
+            return error.AddressNotInAnyHHDM;
+        }
 
-    pub inline fn moveBackward(self: VirtAddr, size: core.Size) VirtAddr {
-        return .{ .value = self.value - size.bytes };
-    }
+        pub inline fn isAligned(self: Self, alignment: core.Size) bool {
+            return std.mem.isAligned(self.value, alignment.bytes);
+        }
 
-    pub inline fn moveBackwardInPlace(self: *VirtAddr, size: core.Size) void {
-        self.value -= size.bytes;
-    }
+        pub inline fn moveForward(self: Self, size: core.Size) Self {
+            return .{ .value = self.value + size.bytes };
+        }
 
-    pub inline fn lessThan(self: VirtAddr, other: VirtAddr) bool {
-        return self.value < other.value;
-    }
+        pub inline fn moveForwardInPlace(self: *Self, size: core.Size) void {
+            self.value += size.bytes;
+        }
 
-    pub inline fn lessThanOrEqual(self: VirtAddr, other: VirtAddr) bool {
-        return self.value <= other.value;
-    }
+        pub inline fn moveBackward(self: Self, size: core.Size) Self {
+            return .{ .value = self.value - size.bytes };
+        }
 
-    pub inline fn greaterThan(self: VirtAddr, other: VirtAddr) bool {
-        return self.value > other.value;
-    }
+        pub inline fn moveBackwardInPlace(self: *Self, size: core.Size) void {
+            self.value -= size.bytes;
+        }
 
-    pub inline fn greaterThanOrEqual(self: VirtAddr, other: VirtAddr) bool {
-        return self.value >= other.value;
-    }
+        pub inline fn lessThan(self: Self, other: Self) bool {
+            return self.value < other.value;
+        }
 
-    pub inline fn equal(self: VirtAddr, other: VirtAddr) bool {
-        return self.value == other.value;
-    }
+        pub inline fn lessThanOrEqual(self: Self, other: Self) bool {
+            return self.value <= other.value;
+        }
 
-    pub fn format(
-        self: VirtAddr,
-        comptime fmt: []const u8,
-        options: std.fmt.FormatOptions,
-        writer: anytype,
-    ) !void {
-        _ = fmt;
-        _ = options;
+        pub inline fn greaterThan(self: Self, other: Self) bool {
+            return self.value > other.value;
+        }
 
-        try writer.writeAll("VirtAddr{ 0x");
-        try std.fmt.formatInt(self.value, 16, .lower, .{ .width = 16, .fill = '0' }, writer);
-        try writer.writeAll(" }");
-    }
+        pub inline fn greaterThanOrEqual(self: Self, other: Self) bool {
+            return self.value >= other.value;
+        }
 
-    comptime {
-        std.debug.assert(@sizeOf(VirtAddr) == @sizeOf(usize));
-        std.debug.assert(@bitSizeOf(VirtAddr) == @bitSizeOf(usize));
-    }
-};
+        pub inline fn equal(self: Self, other: Self) bool {
+            return self.value == other.value;
+        }
+
+        pub fn format(
+            self: Self,
+            comptime fmt: []const u8,
+            options: std.fmt.FormatOptions,
+            writer: anytype,
+        ) !void {
+            _ = fmt;
+            _ = options;
+
+            const name = switch (addr_type) {
+                .phys => "PhysAddr",
+                .virt => "VirtAddr",
+            };
+
+            try writer.writeAll(comptime name ++ "{ 0x");
+            try std.fmt.formatInt(self.value, 16, .lower, .{ .width = 16, .fill = '0' }, writer);
+            try writer.writeAll(" }");
+        }
+    };
+}
+
+comptime {
+    std.debug.assert(@sizeOf(PhysAddr) == @sizeOf(usize));
+    std.debug.assert(@bitSizeOf(PhysAddr) == @bitSizeOf(usize));
+}
+
+comptime {
+    std.debug.assert(@sizeOf(VirtAddr) == @sizeOf(usize));
+    std.debug.assert(@bitSizeOf(VirtAddr) == @bitSizeOf(usize));
+}
 
 pub const PhysRange = struct {
     addr: PhysAddr,
