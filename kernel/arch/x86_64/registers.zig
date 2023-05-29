46cfecdc7d7b9115e7c5c32985f4baaa68d40630
Lee Cannon <leecannon@leecannon.xyz>
2023-05-29 19:06:12 +0100

    kernel/x86_64/registers: add `EFER` abstraction

--- kernel/arch/x86_64/registers.zig
+++ kernel/arch/x86_64/registers.zig
@@ -117,6 +117,55 @@ pub const Cr3 = struct {
     }
 };
 
+/// Extended Feature Enable Register (EFER)
+pub const EFER = packed struct(u64) {
+    syscall_enable: bool,
+
+    _reserved1_7: u7,
+
+    long_mode_enable: bool,
+
+    _reserved9: u1,
+
+    long_mode_active: bool,
+
+    no_execute_enable: bool,
+
+    secure_virtual_machine_enable: bool,
+
+    long_mode_segment_limit_enable: bool,
+
+    fast_fxsave_fxrstor: bool,
+
+    translation_cache_extension: bool,
+
+    _reserved16: u1,
+
+    mcommit_instruction_enable: bool,
+
+    interruptible_wb_enable: bool,
+
+    _reserved19: u1,
+
+    upper_address_ingore_enable: bool,
+
+    automatic_ibrs_enable: bool,
+
+    _reserved22_63: u42,
+
+    pub inline fn read() EFER {
+        return @bitCast(EFER, msr.read());
+    }
+
+    pub inline fn write(self: EFER) void {
+        msr.write(@bitCast(u64, self));
+    }
+
+    const msr = MSR(u64, 0xC0000080);
+
+    pub const format = core.formatStructIgnoreReserved;
+};
+
 pub fn MSR(comptime T: type, comptime register: u32) type {
     return struct {
         pub inline fn read() T {
