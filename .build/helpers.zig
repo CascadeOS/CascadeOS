e718f47e6980e24769809a705ead5cf97a6b3fd3
Lee Cannon <leecannon@leecannon.xyz>
2023-06-16 20:34:03 +0100

    build: refactor external binary execution into a helper function

--- .build/helpers.zig
+++ .build/helpers.zig
@@ -10,3 +10,16 @@ pub fn fileExists(path: []const u8) bool {
     std.fs.cwd().access(path, .{}) catch return false;
     return true;
 }
+
+pub fn runExternalBinary(allocator: std.mem.Allocator, args: []const []const u8, cwd: ?[]const u8) !void {
+    var child = std.ChildProcess.init(args, allocator);
+    if (cwd) |c| child.cwd = c;
+
+    try child.spawn();
+    const term = try child.wait();
+
+    switch (term) {
+        .Exited => |code| if (code != 0) return error.UncleanExit,
+        else => return error.UncleanExit,
+    }
+}
