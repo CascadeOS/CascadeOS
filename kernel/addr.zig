7ed050f94ab3c8c3d505ace6c882ccb88fdc09ee
Lee Cannon <leecannon@leecannon.xyz>
2023-06-17 20:46:52 +0100

    kernel/addr: refactor addr and range implementations into mixins

--- kernel/addr.zig
+++ kernel/addr.zig
@@ -4,63 +4,80 @@ const std = @import("std");
 const core = @import("core");
 const kernel = @import("kernel");
 
-pub const PhysAddr = Addr(.phys);
-pub const VirtAddr = Addr(.virt);
-pub const PhysRange = Range(.phys);
-pub const VirtRange = Range(.virt);
+pub const PhysAddr = extern struct {
+    value: usize,
 
-const Type = enum {
-    phys,
-    virt,
+    const name = "PhysAddr";
+
+    pub inline fn fromInt(value: usize) PhysAddr {
+        // TODO: check that the address is valid (cannoical) https://github.com/CascadeOS/CascadeOS/issues/15
+        return .{ .value = value };
+    }
+
+    pub inline fn toDirectMap(self: PhysAddr) VirtAddr {
+        return .{ .value = self.value + kernel.info.direct_map.addr.value };
+    }
+
+    pub inline fn toNonCachedDirectMap(self: PhysAddr) VirtAddr {
+        return .{ .value = self.value + kernel.info.non_cached_direct_map.addr.value };
+    }
+
+    pub usingnamespace AddrMixin(@This());
+
+    comptime {
+        std.debug.assert(@sizeOf(PhysAddr) == @sizeOf(usize));
+        std.debug.assert(@bitSizeOf(PhysAddr) == @bitSizeOf(usize));
+    }
 };
 
-fn Addr(comptime addr_type: Type) type {
-    return extern struct {
-        value: usize,
+pub const VirtAddr = extern struct {
+    value: usize,
 
-        const Self = @This();
+    const name = "VirtAddr";
 
-        pub const zero: Self = .{ .value = 0 };
+    pub inline fn fromInt(value: usize) VirtAddr {
+        // TODO: check that the address is valid (cannoical) https://github.com/CascadeOS/CascadeOS/issues/15
+        return .{ .value = value };
+    }
 
-        pub inline fn fromInt(value: usize) Self {
-            // TODO: check that the address is valid (cannoical) https://github.com/CascadeOS/CascadeOS/issues/15
-            return .{ .value = value };
-        }
+    pub inline fn fromPtr(ptr: *const anyopaque) VirtAddr {
+        return fromInt(@ptrToInt(ptr));
+    }
 
-        pub inline fn toDirectMap(self: PhysAddr) VirtAddr {
-            return .{ .value = self.value + kernel.info.direct_map.addr.value };
-        }
+    pub inline fn toPtr(self: VirtAddr, comptime PtrT: type) PtrT {
+        return @intToPtr(PtrT, self.value);
+    }
 
-        pub inline fn toNonCachedDirectMap(self: PhysAddr) VirtAddr {
-            return .{ .value = self.value + kernel.info.non_cached_direct_map.addr.value };
-        }
+    /// Returns the physical address of the given diret map virtual address.
+    ///
+    /// ## Safety
+    /// It is the caller's responsibility to ensure that the given virtual address is in the direct map.
+    pub fn unsafeToPhysicalFromDirectMap(self: VirtAddr) PhysAddr {
+        return .{ .value = self.value - kernel.info.direct_map.addr.value };
+    }
 
-        pub inline fn fromPtr(ptr: *const anyopaque) VirtAddr {
-            return fromInt(@ptrToInt(ptr));
-        }
-
-        pub inline fn toPtr(self: VirtAddr, comptime PtrT: type) PtrT {
-            return @intToPtr(PtrT, self.value);
-        }
-
-        /// Returns the physical address of the given diret map virtual address.
-        ///
-        /// ## Safety
-        /// It is the caller's responsibility to ensure that the given virtual address is in the direct map.
-        pub fn unsafeToPhysicalFromDirectMap(self: VirtAddr) PhysAddr {
+    /// Returns the physical address of the given virtual address if it is in one of the direct maps.
+    pub fn toPhysicalFromDirectMap(self: VirtAddr) error{AddressNotInAnyDirectMap}!PhysAddr {
+        if (kernel.info.direct_map.contains(self)) {
             return .{ .value = self.value - kernel.info.direct_map.addr.value };
         }
-
-        /// Returns the physical address of the given virtual address if it is in one of the direct maps.
-        pub fn toPhysicalFromDirectMap(self: VirtAddr) error{AddressNotInAnyDirectMap}!PhysAddr {
-            if (kernel.info.direct_map.contains(self)) {
-                return .{ .value = self.value - kernel.info.direct_map.addr.value };
-            }
-            if (kernel.info.non_cached_direct_map.contains(self)) {
-                return .{ .value = self.value - kernel.info.non_cached_direct_map.addr.value };
-            }
-            return error.AddressNotInAnyDirectMap;
+        if (kernel.info.non_cached_direct_map.contains(self)) {
+            return .{ .value = self.value - kernel.info.non_cached_direct_map.addr.value };
         }
+        return error.AddressNotInAnyDirectMap;
+    }
+
+    pub usingnamespace AddrMixin(@This());
+
+    comptime {
+        std.debug.assert(@sizeOf(VirtAddr) == @sizeOf(usize));
+        std.debug.assert(@bitSizeOf(VirtAddr) == @bitSizeOf(usize));
+    }
+};
+
+fn AddrMixin(comptime Self: type) type {
+    return struct {
+        pub const zero: Self = .{ .value = 0 };
 
         pub inline fn isAligned(self: Self, alignment: core.Size) bool {
             return std.mem.isAligned(self.value, alignment.bytes);
@@ -82,6 +99,10 @@ fn Addr(comptime addr_type: Type) type {
             self.value -= size.bytes;
         }
 
+        pub inline fn equal(self: Self, other: Self) bool {
+            return self.value == other.value;
+        }
+
         pub inline fn lessThan(self: Self, other: Self) bool {
             return self.value < other.value;
         }
@@ -98,17 +119,8 @@ fn Addr(comptime addr_type: Type) type {
             return self.value >= other.value;
         }
 
-        pub inline fn equal(self: Self, other: Self) bool {
-            return self.value == other.value;
-        }
-
         pub fn print(self: Self, writer: anytype) !void {
-            const name = switch (addr_type) {
-                .phys => "PhysAddr",
-                .virt => "VirtAddr",
-            };
-
-            try writer.writeAll(comptime name ++ "{ 0x");
+            try writer.writeAll(comptime Self.name ++ "{ 0x");
             try std.fmt.formatInt(self.value, 16, .lower, .{ .width = 16, .fill = '0' }, writer);
             try writer.writeAll(" }");
         }
@@ -126,57 +138,53 @@ fn Addr(comptime addr_type: Type) type {
     };
 }
 
-comptime {
-    std.debug.assert(@sizeOf(PhysAddr) == @sizeOf(usize));
-    std.debug.assert(@bitSizeOf(PhysAddr) == @bitSizeOf(usize));
-}
+pub const PhysRange = struct {
+    addr: PhysAddr,
+    size: core.Size,
 
-comptime {
-    std.debug.assert(@sizeOf(VirtAddr) == @sizeOf(usize));
-    std.debug.assert(@bitSizeOf(VirtAddr) == @bitSizeOf(usize));
-}
+    const name = "PhysRange";
 
-fn Range(comptime addr_type: Type) type {
-    return extern struct {
-        addr: AddrType,
-        size: core.Size,
-
-        pub const AddrType: type = switch (addr_type) {
-            .phys => PhysAddr,
-            .virt => VirtAddr,
+    pub inline fn toDirectMap(self: PhysRange) VirtRange {
+        return .{
+            .addr = self.addr.toDirectMap(),
+            .size = self.size,
         };
+    }
 
-        const Self = @This();
+    pub usingnamespace RangeMixin(@This(), PhysAddr);
+};
 
+pub const VirtRange = struct {
+    addr: VirtAddr,
+    size: core.Size,
+
+    const name = "VirtRange";
+
+    pub fn fromSlice(slice: anytype) VirtRange {
+        const info: std.builtin.Type = @typeInfo(@TypeOf(slice));
+        if (info != .Pointer) @compileError(""); // TODO: Add error message
+        const pointer_info: std.builtin.Type.Pointer = info.Pointer;
+        if (pointer_info.size != .Slice) @compileError(""); // TODO: Add error message
+        return .{
+            .addr = VirtAddr.fromPtr(slice.ptr),
+            .size = core.Size.from(@sizeOf(pointer_info.child) * slice.len, .byte),
+        };
+    }
+
+    pub fn toSlice(self: VirtRange, comptime T: type) ![]T {
+        const len = try std.math.divExact(usize, self.size.bytes, @sizeOf(T));
+        return self.addr.toPtr([*]T)[0..len];
+    }
+
+    pub usingnamespace RangeMixin(@This(), VirtAddr);
+};
+
+fn RangeMixin(comptime Self: type, comptime AddrType: type) type {
+    return struct {
         pub inline fn fromAddr(addr: AddrType, size: core.Size) Self {
             return .{
                 .addr = addr,
                 .size = size,
-            };
-        }
-
-        pub fn fromSlice(slice: anytype) VirtRange {
-            if (addr_type == .phys) @compileError("`fromSlice` is only implemented for VirtRange");
-            const info: std.builtin.Type = @typeInfo(@TypeOf(slice));
-            if (info != .Pointer) @compileError("");
-            const pointer_info: std.builtin.Type.Pointer = info.Pointer;
-            if (pointer_info.size != .Slice) @compileError("");
-            return .{
-                .addr = AddrType.fromPtr(slice.ptr),
-                .size = core.Size.from(@sizeOf(pointer_info.child) * slice.len, .byte),
-            };
-        }
-
-        pub fn toSlice(self: VirtRange, comptime T: type) ![]T {
-            if (addr_type == .phys) @compileError("`toSlice` is only implemented for VirtRange");
-            const len = try std.math.divExact(usize, self.size.bytes, @sizeOf(T));
-            return self.addr.toPtr([*]T)[0..len];
-        }
-
-        pub inline fn toDirectMap(self: PhysRange) VirtRange {
-            return .{
-                .addr = self.addr.toDirectMap(),
-                .size = self.size,
             };
         }
 
@@ -211,12 +219,7 @@ fn Range(comptime addr_type: Type) type {
         }
 
         pub fn print(value: Self, writer: anytype) !void {
-            const name = switch (addr_type) {
-                .phys => "PhysRange",
-                .virt => "VirtRange",
-            };
-
-            try writer.writeAll(comptime name ++ "{ 0x");
+            try writer.writeAll(Self.name ++ "{ 0x");
             try std.fmt.formatInt(value.addr.value, 16, .lower, .{ .width = 16, .fill = '0' }, writer);
 
             try writer.writeAll(" - 0x");
