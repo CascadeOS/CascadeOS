060b75cc4c3d17b818b029987ca2052c0f20902e
Lee Cannon <leecannon@leecannon.xyz>
2023-07-01 19:46:44 +0100

    kernel/SpinLock: improve documentation

--- kernel/SpinLock.zig
+++ kernel/SpinLock.zig
@@ -6,16 +6,21 @@ const kernel = @import("kernel");
 
 const SpinLock = @This();
 
-z_serving: usize = 1,
-z_allocated: usize = 1,
+/// The ticket whose turn it is to acquire the lock.
+current_ticket: usize = 1,
+
+/// The ticket to hand out next.
+next_available_ticket: usize = 1,
 
 pub const Held = struct {
-    z_enable_interrupts_on_unlock: bool,
-    z_spinlock: *SpinLock,
+    enable_interrupts_on_unlock: bool,
+    spinlock: *SpinLock,
 
+    /// Unlocks the spinlock.
     pub fn unlock(self: Held) void {
-        _ = @atomicRmw(usize, &self.z_spinlock.z_serving, .Add, 1, .Release);
-        if (self.z_enable_interrupts_on_unlock) kernel.arch.interrupts.enableInterrupts();
+        _ = @atomicRmw(usize, &self.spinlock.current_ticket, .Add, 1, .Release);
+
+        if (self.enable_interrupts_on_unlock) kernel.arch.interrupts.enableInterrupts();
     }
 };
 
@@ -25,32 +30,28 @@ pub fn lock(self: *SpinLock) Held {
 
     kernel.arch.interrupts.disableInterrupts();
 
-    self.internalGrab(interrupts_enabled);
+    self.internalGrab();
 
     return .{
-        .z_enable_interrupts_on_unlock = interrupts_enabled,
-        .z_spinlock = self,
+        .enable_interrupts_on_unlock = interrupts_enabled,
+        .spinlock = self,
     };
 }
 
 /// Grab lock without disabling interrupts
 pub fn grab(self: *SpinLock) Held {
-    self.internalGrab(false);
+    self.internalGrab();
     return .{
-        .z_enable_interrupts_on_unlock = false,
-        .z_spinlock = self,
+        .enable_interrupts_on_unlock = false,
+        .spinlock = self,
     };
 }
 
-fn internalGrab(self: *SpinLock, interrupts_were_enabled: bool) void {
-    const ticket = @atomicRmw(usize, &self.z_allocated, .Add, 1, .AcqRel);
+fn internalGrab(self: *SpinLock) void {
+    const ticket = @atomicRmw(usize, &self.next_available_ticket, .Add, 1, .AcqRel);
     while (true) {
-        if (@atomicLoad(usize, &self.z_serving, .Acquire) == ticket) {
+        if (@atomicLoad(usize, &self.current_ticket, .Acquire) == ticket) {
             return;
-        }
-        if (interrupts_were_enabled) {
-            kernel.arch.interrupts.enableInterrupts();
-            kernel.arch.interrupts.disableInterrupts();
         }
         kernel.arch.spinLoopHint();
     }
