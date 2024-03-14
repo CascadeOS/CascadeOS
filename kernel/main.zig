d11f461ce8b7b18cc75eda8455c19dce5631fde3
Lee Cannon <leecannon@leecannon.xyz>
2024-03-14 20:14:07 +0000

    kernel/main: setup early output and output a starting message

--- kernel/main.zig
+++ kernel/main.zig
@@ -8,4 +8,12 @@ const kernel = @import("kernel");
 /// Entry point from the bootloader specific code.
 ///
 /// Only the bootstrap cpu executes this function.
-pub fn kmain() void {}
+pub fn kmain() void {
+    // get output up and running as soon as possible
+    kernel.arch.init.setupEarlyOutput();
+
+    // print starting message
+    if (kernel.arch.init.getEarlyOutput()) |early_output| {
+        early_output.writeAll(comptime "starting CascadeOS " ++ @import("kernel_options").cascade_version ++ "\n") catch {};
+    }
+}
