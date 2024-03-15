571cb9aa6f9886d86c9bed5988183642f3965b70
Lee Cannon <leecannon@leecannon.xyz>
2024-03-15 20:53:53 +0000

    kernel/x86_64: remove superflous inline

--- kernel/arch/x86_64/init.zig
+++ kernel/arch/x86_64/init.zig
@@ -10,24 +10,24 @@ const x86_64 = @import("x86_64.zig");
 var early_output_serial_port: ?x86_64.SerialPort = null;
 
 /// Attempt to set up some form of early output.
-pub fn setupEarlyOutput() callconv(core.inline_in_non_debug_calling_convention) void {
+pub fn setupEarlyOutput() void {
     early_output_serial_port = x86_64.SerialPort.init(.COM1, .Baud115200);
 }
 
 /// Acquire a writer for the early output setup by `setupEarlyOutput`.
-pub fn getEarlyOutput() callconv(core.inline_in_non_debug_calling_convention) ?x86_64.SerialPort.Writer {
+pub fn getEarlyOutput() ?x86_64.SerialPort.Writer {
     return if (early_output_serial_port) |output| output.writer() else null;
 }
 
 /// Prepares the provided `Cpu` for the bootstrap CPU.
 pub fn prepareBootstrapCpu(
     bootstrap_cpu: *kernel.Cpu,
-) callconv(core.inline_in_non_debug_calling_convention) void {
+) void {
     bootstrap_cpu.arch = .{};
 }
 
 /// Load the provided `Cpu` as the current CPU.
-pub fn loadCpu(cpu: *kernel.Cpu) callconv(core.inline_in_non_debug_calling_convention) void { // TODO: TSS, IDT
+pub fn loadCpu(cpu: *kernel.Cpu) void { // TODO: TSS, IDT
     const arch = &cpu.arch;
 
     arch.gdt.load();
