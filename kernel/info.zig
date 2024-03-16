0279577cc7a22c740ae90810650b97ceed39bc44
Lee Cannon <leecannon@leecannon.xyz>
2024-03-16 19:18:43 +0000

    kernel/init: capture the kernel offsets

--- kernel/info.zig
+++ kernel/info.zig
@@ -7,3 +7,15 @@ const kernel = @import("kernel");
 
 // This must be kept in sync with the linker scripts.
 pub const kernel_base_address = core.VirtualAddress.fromInt(0xffffffff80000000);
+
+/// Initialized during `init.captureKernelOffsets.
+pub var kernel_virtual_base_address: core.VirtualAddress = undefined;
+
+/// Initialized during `init.captureKernelOffsets`.
+pub var kernel_physical_base_address: core.PhysicalAddress = undefined;
+
+/// Initialized during `init.captureKernelOffsets`.
+pub var kernel_virtual_slide: ?core.Size = null;
+
+/// Initialized during `init.captureKernelOffsets`.
+pub var kernel_physical_to_virtual_offset: core.Size = undefined;
--- kernel/init.zig
+++ kernel/init.zig
@@ -5,6 +5,8 @@ const std = @import("std");
 const core = @import("core");
 const kernel = @import("kernel");
 
+const log = kernel.log.scoped(.init);
+
 /// Represents the bootstrap cpu during init.
 var bootstrap_cpu: kernel.Cpu = .{
     .id = @enumFromInt(0),
@@ -32,4 +34,23 @@ pub fn kernelInit() !void {
         const starting_message = comptime "starting CascadeOS " ++ @import("kernel_options").cascade_version ++ "\n";
         early_output.writeAll(starting_message) catch {};
     }
+
+    try captureKernelOffsets();
+}
+
+fn captureKernelOffsets() !void {
+    const kernel_base_address = kernel.boot.kernelBaseAddress() orelse return error.KernelBaseAddressNotProvided;
+
+    const kernel_virtual = kernel_base_address.virtual;
+    const kernel_physical = kernel_base_address.physical;
+
+    kernel.info.kernel_virtual_base_address = kernel_virtual;
+    kernel.info.kernel_physical_base_address = kernel_physical;
+    log.debug("kernel virtual base address: {}", .{kernel.info.kernel_virtual_base_address});
+    log.debug("kernel physical base address: {}", .{kernel.info.kernel_physical_base_address});
+
+    kernel.info.kernel_virtual_slide = core.Size.from(kernel_virtual.value - kernel.info.kernel_base_address.value, .byte);
+    kernel.info.kernel_physical_to_virtual_offset = core.Size.from(kernel_virtual.value - kernel_physical.value, .byte);
+    log.debug("kernel virtual slide: 0x{x}", .{kernel.info.kernel_virtual_slide.?.value});
+    log.debug("kernel physical to virtual offset: 0x{x}", .{kernel.info.kernel_physical_to_virtual_offset.value});
 }
