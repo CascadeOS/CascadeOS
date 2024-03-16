c3f44ae7d0dec9d1301c29eedf15c3c5b628e439
Lee Cannon <leecannon@leecannon.xyz>
2024-03-16 19:19:45 +0000

    kernel/init: capture the direct maps

--- kernel/info.zig
+++ kernel/info.zig
@@ -19,3 +19,15 @@ pub var kernel_virtual_slide: ?core.Size = null;
 
 /// Initialized during `init.captureKernelOffsets`.
 pub var kernel_physical_to_virtual_offset: core.Size = undefined;
+
+/// This direct map provides an identity mapping between virtual and physical addresses.
+///
+/// Initialized during `init.captureDirectMaps`.
+pub var direct_map: core.VirtualRange = undefined;
+
+/// This direct map provides an identity mapping between virtual and physical addresses.
+///
+/// The page tables used disable caching for this range.
+///
+/// Initialized during `init.captureDirectMaps`.
+pub var non_cached_direct_map: core.VirtualRange = undefined;
--- kernel/init.zig
+++ kernel/init.zig
@@ -36,6 +36,7 @@ pub fn kernelInit() !void {
     }
 
     try captureKernelOffsets();
+    try captureDirectMaps();
 }
 
 fn captureKernelOffsets() !void {
@@ -53,4 +54,83 @@ fn captureKernelOffsets() !void {
     kernel.info.kernel_physical_to_virtual_offset = core.Size.from(kernel_virtual.value - kernel_physical.value, .byte);
     log.debug("kernel virtual slide: 0x{x}", .{kernel.info.kernel_virtual_slide.?.value});
     log.debug("kernel physical to virtual offset: 0x{x}", .{kernel.info.kernel_physical_to_virtual_offset.value});
+}
+
+fn captureDirectMaps() !void {
+    log.debug("capturing direct maps", .{});
+
+    const direct_map_size = try calculateLengthOfDirectMap();
+
+    kernel.info.direct_map = try calculateDirectMapRange(direct_map_size);
+    log.debug("direct map: {}", .{kernel.info.direct_map});
+
+    kernel.info.non_cached_direct_map = try calculateNonCachedDirectMapRange(direct_map_size, kernel.info.direct_map);
+    log.debug("non-cached direct map: {}", .{kernel.info.non_cached_direct_map});
+}
+
+/// Calculates the length of the direct map.
+fn calculateLengthOfDirectMap() !core.Size {
+    var memory_map_iterator = kernel.boot.memoryMap(.backwards);
+
+    const last_usable_entry: kernel.boot.MemoryMapEntry = blk: {
+        // search from the end of the memory map for the last usable region
+
+        while (memory_map_iterator.next()) |entry| {
+            if (entry.type == .reserved_or_unusable) continue;
+
+            break :blk entry;
+        }
+
+        return error.NoUsableMemoryRegions;
+    };
+
+    const initial_size = core.Size.from(last_usable_entry.range.end().value, .byte);
+
+    // We align the length of the direct map to `largest_page_size` to allow large pages to be used for the mapping.
+    var aligned_size = initial_size.alignForward(
+        kernel.arch.paging.all_page_sizes[kernel.arch.paging.all_page_sizes.len - 1],
+    );
+
+    // We ensure that the lowest 4GiB are always mapped.
+    const four_gib = core.Size.from(4, .gib);
+    if (aligned_size.lessThan(four_gib)) aligned_size = four_gib;
+
+    log.debug("size of direct map: {}", .{aligned_size});
+
+    return aligned_size;
+}
+
+fn calculateDirectMapRange(direct_map_size: core.Size) !core.VirtualRange {
+    const direct_map_address = kernel.boot.directMapAddress() orelse return error.DirectMapAddressNotProvided;
+
+    if (!direct_map_address.isAligned(kernel.arch.paging.standard_page_size)) {
+        return error.DirectMapAddressNotAligned;
+    }
+
+    return core.VirtualRange.fromAddr(direct_map_address, direct_map_size);
+}
+
+fn calculateNonCachedDirectMapRange(
+    direct_map_size: core.Size,
+    direct_map_range: core.VirtualRange,
+) !core.VirtualRange {
+    // try to place the non-cached direct map directly _before_ the direct map
+    {
+        const candidate_range = direct_map_range.moveBackward(direct_map_size);
+        // check that we have not gone below the higher half
+        if (candidate_range.address.greaterThanOrEqual(kernel.arch.paging.higher_half)) {
+            return candidate_range;
+        }
+    }
+
+    // try to place the non-cached direct map directly _after_ the direct map
+    {
+        const candidate_range = direct_map_range.moveForward(direct_map_size);
+        // check that we are not overlapping with the kernel
+        if (!candidate_range.contains(kernel.info.kernel_virtual_base_address)) {
+            return candidate_range;
+        }
+    }
+
+    return error.NoUsableMemoryRegions;
 }
