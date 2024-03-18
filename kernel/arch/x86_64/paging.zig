e84b411b5577dd79425ff922d702ef0413e3b3b6
Lee Cannon <leecannon@leecannon.xyz>
2024-03-18 19:24:52 +0000

    kernel/x86_64: handle errors in `mapToPhysicalRangeAllPageSizes`

--- kernel/arch/x86_64/paging.zig
+++ kernel/arch/x86_64/paging.zig
@@ -13,14 +13,20 @@ const PageTable = x86_64.PageTable;
 const MapType = kernel.vmm.MapType;
 const MapError = kernel.arch.paging.MapError;
 
+/// This is the total size of the virtual address space that one entry in the 4th level of the page table covers.
+const size_of_level4_entry = core.Size.from(0x8000000000, .byte);
+
 /// Maps the `virtual_range` to the `physical_range` with mapping type given by `map_type`.
 ///
 /// Caller must ensure:
 ///  - the virtual range address and size are aligned to the standard page size
 ///  - the physical range address and size are aligned to the standard page size
 ///  - the virtual range size is equal to the physical range size
+///  - the virtual range is not already mapped
 ///
 /// This function is allowed to use all page sizes available to the architecture.
+///
+/// This function does not handle flushing the TLB.
 pub fn mapToPhysicalRangeAllPageSizes(
     page_table: *PageTable,
     virtual_range: core.VirtualRange,
@@ -39,6 +45,11 @@ pub fn mapToPhysicalRangeAllPageSizes(
     const end_virtual_address = virtual_range.end();
     var current_physical_address = physical_range.address;
     var size_remaining = virtual_range.size;
+
+    errdefer {
+        // it is correct to unmap the whole range as the caller must ensure that the range is not already mapped
+        unmapAnyPageSizes(page_table, virtual_range);
+    }
 
     var gib_page_mappings: usize = 0;
     var mib_page_mappings: usize = 0;
@@ -115,6 +126,87 @@ pub fn mapToPhysicalRangeAllPageSizes(
     );
 }
 
+/// Unmaps the `virtual_range`, supports unmapping any page size including non-hetrogeneous page sizes.
+///
+/// Any mappings that overlap with the `virtual_range` will be unmapped.
+///
+/// Callers must ensure:
+/// - the virtual range address and size are aligned to the standard page size
+///
+/// This function does not handle flushing the TLB.
+fn unmapAnyPageSizes(page_table: *PageTable, virtual_range: core.VirtualRange) void {
+    core.debugAssert(virtual_range.address.isAligned(kernel.arch.paging.standard_page_size));
+    core.debugAssert(virtual_range.size.isAligned(kernel.arch.paging.standard_page_size));
+
+    // TODO: this function is not correct, for example once all level 1 pages in a level 2 page are unmapped and the
+    //       entire level 2 page is within the virtual range then the level 2 page should also be unmapped.
+    //
+    //       the same issue is true at the other levels, maybe calculating start and end indexes and iterating the
+    //       tables directly will make it easier to do correctly?
+
+    var current_virtual_address = virtual_range.address;
+    const end_virtual_address = virtual_range.end();
+
+    while (current_virtual_address.lessThan(end_virtual_address)) {
+        const level4_entry = page_table.getEntryLevel4(current_virtual_address);
+
+        if (!level4_entry.present.read()) {
+            current_virtual_address.moveForwardInPlace(PageTable.small_page_size);
+            current_virtual_address.alignForwardInPlace(size_of_level4_entry);
+            continue;
+        }
+
+        const level3_table = level4_entry.getNextLevel(kernel.directMapFromPhysical) catch unreachable; // checked above
+        const level3_entry = level3_table.getEntryLevel3(current_virtual_address);
+
+        if (!level3_entry.present.read()) {
+            current_virtual_address.moveForwardInPlace(PageTable.small_page_size);
+            current_virtual_address.alignForwardInPlace(PageTable.large_page_size);
+            continue;
+        }
+
+        if (level3_entry.huge.read()) {
+            const address = level3_entry.getAddress4kib();
+            level3_entry.zero();
+            kernel.pmm.deallocatePage(address.toRange(PageTable.small_page_size));
+
+            current_virtual_address.moveForwardInPlace(PageTable.small_page_size);
+            current_virtual_address.alignForwardInPlace(PageTable.large_page_size);
+            continue;
+        }
+
+        const level2_table = level3_entry.getNextLevel(kernel.directMapFromPhysical) catch unreachable; // checked above
+        const level2_entry = level2_table.getEntryLevel2(current_virtual_address);
+
+        if (!level2_entry.present.read()) {
+            current_virtual_address.moveForwardInPlace(PageTable.small_page_size);
+            current_virtual_address.alignForwardInPlace(PageTable.medium_page_size);
+            continue;
+        }
+
+        if (level2_entry.huge.read()) {
+            const address = level2_entry.getAddress4kib();
+            level2_entry.zero();
+            kernel.pmm.deallocatePage(address.toRange(PageTable.small_page_size));
+
+            current_virtual_address.moveForwardInPlace(PageTable.small_page_size);
+            current_virtual_address.alignForwardInPlace(PageTable.medium_page_size);
+            continue;
+        }
+
+        const level1_table = level2_entry.getNextLevel(kernel.directMapFromPhysical) catch unreachable; // checked above
+        const level1_entry = level1_table.getEntryLevel1(current_virtual_address);
+
+        if (!level1_entry.present.read()) {
+            current_virtual_address.moveForwardInPlace(PageTable.small_page_size);
+            continue;
+        }
+
+        level1_entry.zero();
+        current_virtual_address.moveForwardInPlace(PageTable.small_page_size);
+    }
+}
+
 /// Maps a 1 GiB page.
 fn mapTo1GiB(
     level4_table: *PageTable,
@@ -134,11 +226,9 @@ fn mapTo1GiB(
     );
     errdefer {
         if (created_level3_table) {
+            const address = level4_entry.getAddress4kib();
             level4_entry.zero();
-            kernel.pmm.deallocatePage(
-                kernel.physicalFromDirectMapUnsafe(core.VirtualAddress.fromPtr(level3_table))
-                    .toRange(PageTable.small_page_size),
-            );
+            kernel.pmm.deallocatePage(address.toRange(PageTable.small_page_size));
         }
     }
 
@@ -169,11 +259,9 @@ fn mapTo2MiB(
     );
     errdefer {
         if (created_level3_table) {
+            const address = level4_entry.getAddress4kib();
             level4_entry.zero();
-            kernel.pmm.deallocatePage(
-                kernel.physicalFromDirectMapUnsafe(core.VirtualAddress.fromPtr(level3_table))
-                    .toRange(PageTable.small_page_size),
-            );
+            kernel.pmm.deallocatePage(address.toRange(PageTable.small_page_size));
         }
     }
 
@@ -185,11 +273,9 @@ fn mapTo2MiB(
     );
     errdefer {
         if (created_level2_table) {
+            const address = level3_entry.getAddress4kib();
             level3_entry.zero();
-            kernel.pmm.deallocatePage(
-                kernel.physicalFromDirectMapUnsafe(core.VirtualAddress.fromPtr(level2_table))
-                    .toRange(PageTable.small_page_size),
-            );
+            kernel.pmm.deallocatePage(address.toRange(PageTable.small_page_size));
         }
     }
 
@@ -220,11 +306,9 @@ fn mapTo4KiB(
     );
     errdefer {
         if (created_level3_table) {
+            const address = level4_entry.getAddress4kib();
             level4_entry.zero();
-            kernel.pmm.deallocatePage(
-                kernel.physicalFromDirectMapUnsafe(core.VirtualAddress.fromPtr(level3_table))
-                    .toRange(PageTable.small_page_size),
-            );
+            kernel.pmm.deallocatePage(address.toRange(PageTable.small_page_size));
         }
     }
 
@@ -236,11 +320,9 @@ fn mapTo4KiB(
     );
     errdefer {
         if (created_level2_table) {
+            const address = level3_entry.getAddress4kib();
             level3_entry.zero();
-            kernel.pmm.deallocatePage(
-                kernel.physicalFromDirectMapUnsafe(core.VirtualAddress.fromPtr(level2_table))
-                    .toRange(PageTable.small_page_size),
-            );
+            kernel.pmm.deallocatePage(address.toRange(PageTable.small_page_size));
         }
     }
 
@@ -252,11 +334,9 @@ fn mapTo4KiB(
     );
     errdefer {
         if (created_level1_table) {
+            const address = level2_entry.getAddress4kib();
             level2_entry.zero();
-            kernel.pmm.deallocatePage(
-                kernel.physicalFromDirectMapUnsafe(core.VirtualAddress.fromPtr(level1_table))
-                    .toRange(PageTable.small_page_size),
-            );
+            kernel.pmm.deallocatePage(address.toRange(PageTable.small_page_size));
         }
     }
 
