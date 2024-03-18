7876497e710028b759fd5dd28723e3370b718d70
Lee Cannon <leecannon@leecannon.xyz>
2024-03-18 00:08:20 +0000

    kernel/info: add function to fetch the sdf data slice

--- kernel/info.zig
+++ kernel/info.zig
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2024 Lee Cannon <leecannon@leecannon.xyz>
 
+//! Runtime discovered/calculated values.
+
 const std = @import("std");
 const core = @import("core");
 const kernel = @import("kernel");
@@ -31,3 +33,24 @@ pub var direct_map: core.VirtualRange = undefined;
 ///
 /// Initialized during `init.captureDirectMaps`.
 pub var non_cached_direct_map: core.VirtualRange = undefined;
+
+pub fn sdfSlice() []const u8 {
+    const static = struct {
+        const sdf = @import("sdf");
+
+        var opt_sdf_slice: ?[]const u8 = null;
+        extern const __sdf_start: u8;
+    };
+
+    if (static.opt_sdf_slice) |s| return s;
+
+    const ptr: [*]const u8 = @ptrCast(&static.__sdf_start);
+    var fbs = std.io.fixedBufferStream(ptr[0..@sizeOf(static.sdf.Header)]);
+
+    const header = static.sdf.Header.read(fbs.reader()) catch core.panic("SDF data is invalid");
+
+    const slice = ptr[0..header.total_size_of_sdf_data];
+
+    static.opt_sdf_slice = slice;
+    return slice;
+}
