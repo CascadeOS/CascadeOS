96ebf74cbefb8e263cf3f719c32f094e3cdff3f9
Lee Cannon <leecannon@leecannon.xyz>
2024-04-07 14:48:44 +0100

    kernel/scheduler: use a `SinglyLinkedFIFO` for the ready to run queue

--- kernel/Thread.zig
+++ kernel/Thread.zig
@@ -4,6 +4,7 @@
 const core = @import("core");
 const kernel = @import("kernel");
 const std = @import("std");
+const containers = @import("containers");
 
 const Thread = @This();
 
@@ -19,7 +20,10 @@ process: ?*kernel.Process,
 
 kernel_stack: kernel.Stack,
 
-next_thread: ?*Thread = null,
+/// Used to track the next thread in any linked list.
+///
+/// Used in the ready queue.
+next_thread_node: containers.SingleNode = .{},
 
 pub fn name(self: *const Thread) []const u8 {
     return self._name.constSlice();
@@ -27,6 +31,10 @@ pub fn name(self: *const Thread) []const u8 {
 
 pub inline fn isKernel(self: *const Thread) bool {
     return self.process == null;
+}
+
+pub inline fn fromNode(node: *containers.SingleNode) *Thread {
+    return @fieldParentPtr("next_thread_node", node);
 }
 
 pub fn print(thread: *const Thread, writer: std.io.AnyWriter, indent: usize) !void {
--- kernel/scheduler.zig
+++ kernel/scheduler.zig
@@ -4,12 +4,12 @@
 const std = @import("std");
 const core = @import("core");
 const kernel = @import("kernel");
+const containers = @import("containers");
 
 const log = kernel.log.scoped(.scheduler);
 
 var lock: kernel.sync.TicketSpinLock = .{};
-var ready_to_run_start: ?*kernel.Thread = null;
-var ready_to_run_end: ?*kernel.Thread = null;
+var ready_to_run: containers.SinglyLinkedFIFO = .{};
 
 /// Performs a round robin scheduling of the ready threads.
 ///
@@ -29,17 +29,12 @@ pub fn schedule(held: kernel.sync.TicketSpinLock.Held) void {
 
     const opt_current_thread = cpu.current_thread;
 
-    const new_thread = ready_to_run_start orelse {
+    const new_thread_node = ready_to_run.pop() orelse {
         // no thread to run
         switchToIdle(cpu, opt_current_thread);
         unreachable;
     };
-
-    // update the ready queue
-    ready_to_run_start = new_thread.next_thread;
-    if (new_thread == ready_to_run_end) ready_to_run_end = null;
-
-    new_thread.next_thread = null;
+    const new_thread = kernel.Thread.fromNode(new_thread_node);
 
     const current_thread = opt_current_thread orelse {
         // we were previously idle
@@ -60,17 +55,10 @@ pub fn schedule(held: kernel.sync.TicketSpinLock.Held) void {
 /// This function must be called with the lock held (see `lockScheduler`).
 pub fn queueThread(held: kernel.sync.TicketSpinLock.Held, thread: *kernel.Thread) void {
     validateLock(held);
-    core.debugAssert(thread.next_thread == null);
+    core.debugAssert(thread.next_thread_node.next == null);
 
     thread.state = .ready;
-
-    if (ready_to_run_end) |last_thread| {
-        last_thread.next_thread = thread;
-        ready_to_run_end = thread;
-    } else {
-        ready_to_run_start = thread;
-        ready_to_run_end = thread;
-    }
+    ready_to_run.push(&thread.next_thread_node);
 }
 
 fn switchToIdle(cpu: *kernel.Cpu, opt_current_thread: ?*kernel.Thread) noreturn {
@@ -141,10 +129,10 @@ fn idle() noreturn {
     log.debug("entering idle", .{});
 
     while (true) {
-        if (ready_to_run_start != null) {
+        if (!ready_to_run.isEmpty()) {
             const held = kernel.scheduler.lockScheduler();
             defer held.release();
-            if (ready_to_run_start != null) schedule(held);
+            if (!ready_to_run.isEmpty()) schedule(held);
         }
         kernel.arch.halt();
     }
