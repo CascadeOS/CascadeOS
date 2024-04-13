53275637ad629fe156afed26d64d77236ce924b5
Lee Cannon <leecannon@leecannon.xyz>
2024-04-13 19:39:23 +0100

    kernel/vmm/DirectMapPool: merge `BucketHeader` into `Bucket`
    This is a bit annoying as the bit set is variable length meaning so
    annoying self-referential limitations get hit when calculating the
    number of items in a bucket.

--- kernel/vmm/DirectMapPool.zig
+++ kernel/vmm/DirectMapPool.zig
@@ -75,70 +75,70 @@ pub fn DirectMapPool(
             const held = self.mutex.acquire();
             defer held.release();
 
-            const bucket_header = blk: {
+            const bucket = blk: {
                 if (self.available_buckets.peek()) |bucket_node|
-                    break :blk BucketHeader.fromNode(bucket_node);
+                    break :blk Bucket.fromNode(bucket_node);
 
                 // no buckets available, allocate a new one
 
                 break :blk try self.allocateNewBucket();
             };
 
-            return self.getItemFromBucket(bucket_header) orelse
+            return self.getItemFromBucket(bucket) orelse
                 unreachable; // bucket is either freshly allocated or in available list so there must be a free item
         }
 
         pub fn give(self: *Self, item: *T) void {
-            const bucket_header = Bucket.getHeader(item);
-            const bit_index = bucket_header.getIndex(item);
+            const bucket = Bucket.getBucketFromItemPtr(item);
+            const bit_index = bucket.getIndexFromItemPtr(item);
 
             const held = self.mutex.acquire();
             defer held.release();
 
-            bucket_header.bitset.set(bit_index);
-            log.debug("added item at index {} in {} to the pool", .{ bit_index, bucket_header });
+            bucket.bitset.set(bit_index);
+            log.debug("added item at index {} in {} to the pool", .{ bit_index, bucket });
 
-            if (bucket_header.empty) {
+            if (bucket.empty) {
                 // bucket was previously empty, move it to the available list
 
-                self.addBucketToAvailableBuckets(bucket_header);
+                self.addBucketToAvailableBuckets(bucket);
 
-                log.debug("moved previously empty {} to available list", .{bucket_header});
+                log.debug("moved previously empty {} to available list", .{bucket});
 
                 return;
             }
 
-            if (bucket_header.bitset.allSet()) {
-                log.debug("deallocating {}", .{bucket_header});
+            if (bucket.bitset.allSet()) {
+                log.debug("deallocating {}", .{bucket});
 
                 // bucket is full, no items are in use
-                self.deallocateBucket(bucket_header);
+                self.deallocateBucket(bucket);
             }
         }
 
-        fn getItemFromBucket(self: *Self, bucket_header: *BucketHeader) ?*T {
-            const index = bucket_header.bitset.toggleFirstSet() orelse return null;
+        fn getItemFromBucket(self: *Self, bucket: *Bucket) ?*T {
+            const index = bucket.bitset.toggleFirstSet() orelse return null;
 
-            if (bucket_header.bitset.allUnset()) {
+            if (bucket.bitset.allUnset()) {
                 // the bucket is empty so we remove it from the available list
 
-                self.removeBucketFromAvailableBuckets(bucket_header);
+                self.removeBucketFromAvailableBuckets(bucket);
 
-                log.debug("removed empty {} from available list", .{bucket_header});
+                log.debug("removed empty {} from available list", .{bucket});
             }
 
-            log.debug("provided item at index {} in {}", .{ index, bucket_header });
+            log.debug("provided item at index {} in {}", .{ index, bucket });
 
-            return &bucket_header.bucket.?.items[index];
+            return &bucket.items[index];
         }
 
-        fn allocateNewBucket(self: *Self) GetError!*BucketHeader {
+        fn allocateNewBucket(self: *Self) GetError!*Bucket {
             var is_new_bucket_group = false;
 
             const bucket_group = blk: {
                 if (self.bucket_group_table.len != 0) {
                     const last_bucket_group = self.bucket_group_table.buffer[self.bucket_group_table.len - 1];
-                    if (last_bucket_group.headers.len < last_bucket_group.headers.capacity()) {
+                    if (last_bucket_group.buckets.len < last_bucket_group.buckets.capacity()) {
                         // the last bucket group in the `bucket_group_table` has room for another bucket
                         break :blk last_bucket_group;
                     }
@@ -161,75 +161,76 @@ pub fn DirectMapPool(
                 _ = self.bucket_group_table.pop();
             };
 
-            const bucket_header = bucket_group.headers.addOne() catch {
+            const bucket_ptr = bucket_group.buckets.addOne() catch {
                 // the block above ensures that the bucket group has room for another bucket or is a freshly
                 // allocated bucket group.
                 unreachable;
             };
-            errdefer _ = bucket_group.headers.pop();
+            errdefer _ = bucket_group.buckets.pop();
 
-            const bucket = try Bucket.create(bucket_header);
+            const bucket = try Bucket.create(bucket_group);
             errdefer bucket.destroy();
 
-            bucket_header.* = .{
-                .bucket_group = bucket_group,
-                .bucket = bucket,
-            };
+            bucket_ptr.* = bucket;
 
-            self.addBucketToAvailableBuckets(bucket_header);
+            self.addBucketToAvailableBuckets(bucket);
 
             if (is_new_bucket_group) {
-                log.debug("allocated {} in a new {}", .{ bucket_header, bucket_group });
+                log.debug("allocated {} in a new {}", .{ bucket, bucket_group });
             } else {
-                log.debug("allocated {} in {}", .{ bucket_header, bucket_group });
+                log.debug("allocated {} in {}", .{ bucket, bucket_group });
             }
 
-            return bucket_header;
+            return bucket;
         }
 
-        fn deallocateBucket(self: *Self, bucket_header: *BucketHeader) void {
-            core.debugAssert(!bucket_header.empty);
+        fn deallocateBucket(self: *Self, bucket: *Bucket) void {
+            core.debugAssert(!bucket.empty);
 
-            self.removeBucketFromAvailableBuckets(bucket_header);
+            self.removeBucketFromAvailableBuckets(bucket);
 
-            bucket_header.bucket.?.destroy();
-            bucket_header.bucket = null;
+            const bucket_group = bucket.bucket_group;
 
-            const this_bucket_group = bucket_header.bucket_group;
-            for (this_bucket_group.headers.constSlice()) |header| {
-                if (header.bucket != null) return; // bucket still in use in this group
-            }
+            const bucket_index = for (bucket_group.buckets.constSlice(), 0..) |candidate_bucket, i| {
+                if (candidate_bucket == bucket) break i;
+            } else unreachable; // it is not possible for a bucket to not be in `buckets`
+
+            _ = bucket_group.buckets.swapRemove(bucket_index);
+
+            bucket.destroy();
+
+            if (bucket_group.buckets.len != 0) return; // bucket group still contains available buckets
 
             const bucket_group_index = for (self.bucket_group_table.constSlice(), 0..) |candidate_bucket_group, i| {
-                if (candidate_bucket_group == this_bucket_group) break i;
+                if (candidate_bucket_group == bucket_group) break i;
             } else unreachable; // it is not possible for a bucket group to not be in `bucket_group_table`
 
-            this_bucket_group.destroy();
-
             _ = self.bucket_group_table.swapRemove(bucket_group_index);
+
+            bucket_group.destroy();
         }
 
         /// Add a bucket to the available list.
         ///
         /// The caller must have the write lock.
-        fn addBucketToAvailableBuckets(self: *Self, bucket_header: *BucketHeader) void {
-            self.available_buckets.push(&bucket_header.node);
-            bucket_header.empty = false;
+        fn addBucketToAvailableBuckets(self: *Self, bucket: *Bucket) void {
+            self.available_buckets.push(&bucket.node);
+            bucket.empty = false;
         }
 
         /// Remove a bucket from the available list.
         ///
         /// The caller must have the write lock.
-        fn removeBucketFromAvailableBuckets(self: *Self, bucket_header: *BucketHeader) void {
-            core.debugAssert(!bucket_header.empty);
-            self.available_buckets.remove(&bucket_header.node);
-            bucket_header.empty = true;
+        fn removeBucketFromAvailableBuckets(self: *Self, bucket: *Bucket) void {
+            core.debugAssert(!bucket.empty);
+            self.available_buckets.remove(&bucket.node);
+            bucket.empty = true;
         }
 
         const BucketGroup = struct {
-            headers: std.BoundedArray(BucketHeader, number_of_buckets) align(standard_page_size) = .{},
+            buckets: std.BoundedArray(*Bucket, number_of_buckets) align(standard_page_size) = .{},
 
-            pub const number_of_buckets = (standard_page_size - @sizeOf(u8)) / @sizeOf(BucketHeader);
+            pub const number_of_buckets = (standard_page_size - @sizeOf(u8)) / @sizeOf(*Bucket);
 
             pub fn create() !*BucketGroup {
                 const bucket_group: *BucketGroup = @ptrCast(try allocPage());
@@ -252,9 +253,9 @@ pub fn DirectMapPool(
                     .{},
                     writer,
                 );
-                try writer.writeAll(">{ headers: ");
+                try writer.writeAll(">{ buckets: ");
                 try std.fmt.formatInt(
-                    bucket_group.headers.len,
+                    bucket_group.buckets.len,
                     10,
                     .lower,
                     .{},
@@ -287,10 +288,9 @@ pub fn DirectMapPool(
             }
         };
 
-        const BucketHeader = struct {
-            bucket_group: *BucketGroup,
+        const Bucket = struct {
+            bucket_group: *BucketGroup align(standard_page_size),
 
-            bucket: ?*Bucket = null,
             bitset: BucketBitSet = BucketBitSet.initFull(),
 
             node: containers.DoubleNode = .{},
@@ -298,64 +298,21 @@ pub fn DirectMapPool(
             /// If this is true then all items in this bucket are in use.
             empty: bool = false,
 
-            /// Returns the index of the given item in the bucket.
-            ///
-            /// It is the caller's responsibility to ensure that the pointer is in this bucket.
-            pub fn getIndex(self: *const BucketHeader, item: *T) usize {
-                return @divExact(@intFromPtr(item) - @intFromPtr(&self.bucket.?.items), @sizeOf(T));
-            }
-
-            inline fn fromNode(node: *containers.DoubleNode) *BucketHeader {
-                return @fieldParentPtr("node", node);
-            }
-
-            pub fn print(bucket_header: *const BucketHeader, writer: std.io.AnyWriter, indent: usize) !void {
-                _ = indent;
-
-                try writer.writeAll("Bucket<0x");
-                try std.fmt.formatInt(
-                    @intFromPtr(bucket_header),
-                    16,
-                    .lower,
-                    .{},
-                    writer,
-                );
-                try writer.writeAll(">{ empty: ");
-                try writer.print("{}", .{bucket_header.empty});
-                try writer.writeAll(" }");
-            }
-
-            pub inline fn format(
-                bucket_header: *const BucketHeader,
-                comptime fmt: []const u8,
-                options: std.fmt.FormatOptions,
-                writer: anytype,
-            ) !void {
-                _ = options;
-                _ = fmt;
-                return if (@TypeOf(writer) == std.io.AnyWriter)
-                    print(bucket_header, writer, 0)
-                else
-                    print(bucket_header, writer.any(), 0);
-            }
-
-            fn __helpZls() void {
-                BucketHeader.print(undefined, @as(std.fs.File.Writer, undefined), 0);
-            }
-        };
-
-        const Bucket = struct {
-            header: *BucketHeader align(standard_page_size),
             items: [number_of_items]T = undefined,
 
-            const size_of_header_field = if (@sizeOf(*BucketHeader) > @alignOf(T)) @sizeOf(*BucketHeader) else @alignOf(T);
-            pub const number_of_items = (standard_page_size - size_of_header_field) / @sizeOf(T);
+            // TODO: Calculate this.
+            //
+            // We want to do something like `(standard_page_size - @offsetOf(Bucket, "items")) / @sizeOf(T)` but
+            // `BucketBitSet`s size depends on `number_of_items` making this self-referential.
+            pub const number_of_items = 63;
 
-            pub fn create(header: *BucketHeader) !*Bucket {
+            pub fn create(bucket_group: *BucketGroup) !*Bucket {
                 const bucket: *Bucket = @ptrCast(try allocPage());
+
                 bucket.* = .{
-                    .header = header,
+                    .bucket_group = bucket_group,
                 };
+
                 return bucket;
             }
 
@@ -363,18 +320,58 @@ pub fn DirectMapPool(
                 deallocPage(std.mem.asBytes(self));
             }
 
-            /// Returns the header of the bucket that contains the given pointer.
+            /// Returns the the bucket that contains the given pointer.
             ///
             /// It is the caller's responsibility to ensure that the pointer is in a bucket.
-            pub fn getHeader(ptr: *T) *BucketHeader {
-                const bucket: *Bucket = @ptrFromInt(
-                    std.mem.alignBackward(usize, @intFromPtr(ptr), standard_page_size),
+            pub fn getBucketFromItemPtr(item: *T) *Bucket {
+                return @ptrFromInt(
+                    std.mem.alignBackward(usize, @intFromPtr(item), standard_page_size),
                 );
-                return bucket.header;
             }
 
-            pub inline fn format(_: *Bucket, comptime _: []const u8, _: std.fmt.FormatOptions, _: anytype) !void {
-                @compileError("dont format a bucket, format its header");
+            /// Returns the index of the given item in the bucket.
+            ///
+            /// It is the caller's responsibility to ensure that the pointer is in this bucket.
+            pub fn getIndexFromItemPtr(self: *const Bucket, item: *T) usize {
+                return @divExact(@intFromPtr(item) - @intFromPtr(&self.items), @sizeOf(T));
+            }
+
+            inline fn fromNode(node: *containers.DoubleNode) *Bucket {
+                return @alignCast(@fieldParentPtr("node", node));
+            }
+
+            pub fn print(bucket: *const Bucket, writer: std.io.AnyWriter, indent: usize) !void {
+                _ = indent;
+
+                try writer.writeAll("Bucket<0x");
+                try std.fmt.formatInt(
+                    @intFromPtr(bucket),
+                    16,
+                    .lower,
+                    .{},
+                    writer,
+                );
+                try writer.writeAll(">{ empty: ");
+                try writer.print("{}", .{bucket.empty});
+                try writer.writeAll(" }");
+            }
+
+            pub inline fn format(
+                bucket: *const Bucket,
+                comptime fmt: []const u8,
+                options: std.fmt.FormatOptions,
+                writer: anytype,
+            ) !void {
+                _ = options;
+                _ = fmt;
+                return if (@TypeOf(writer) == std.io.AnyWriter)
+                    print(bucket, writer, 0)
+                else
+                    print(bucket, writer.any(), 0);
+            }
+
+            fn __helpZls() void {
+                Bucket.print(undefined, @as(std.fs.File.Writer, undefined), 0);
             }
 
             comptime {
