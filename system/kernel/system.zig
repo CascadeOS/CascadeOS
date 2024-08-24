8058ba56e0318065113156b806235d280c1ffba6
Lee Cannon <leecannon@leecannon.xyz>
2024-08-24 22:20:58 +0100

    kernel/system: centralize kernel memory layout information

--- system/kernel/system.zig
+++ system/kernel/system.zig
@@ -6,13 +6,23 @@
 /// Initialized during init and never modified again.
 pub var executors: std.BoundedArray(kernel.Executor, kernel.config.limits.max_executors) = .{};
 
-/// Returns the executor with the given id.
+/// The memory layout of the kernel.
 ///
-/// It is the caller's responsibility to ensure that the id is valid.
-pub inline fn getExecutor(id: kernel.Executor.Id) *kernel.Executor {
-    std.debug.assert(id != .none);
-    return &executors.slice()[@intFromEnum(id) - 1];
-}
+/// Initialized during `init.buildMemoryLayout`.
+pub var memory_layout: MemoryLayout = .{};
+
+pub const MemoryLayout = struct {
+    /// The virtual base address that the kernel was loaded at.
+    virtual_base_address: core.VirtualAddress = kernel.config.kernel_base_address,
+
+    /// The offset from the requested ELF virtual base address to the address that the kernel was actually loaded at.
+    ///
+    /// This is optional due to the small window on start up where the panic handler can run before this is set.
+    virtual_offset: ?core.Size = null,
+
+    /// Offset from the virtual address of kernel sections to the physical address of the section.
+    physical_to_virtual_offset: ?core.Size = null,
+};
 
 const std = @import("std");
 const core = @import("core");
