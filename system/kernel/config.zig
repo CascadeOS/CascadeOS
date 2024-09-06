59b5c5ed37d92a5b3541f026f9357b74b625e916
Lee Cannon <leecannon@leecannon.xyz>
2024-09-06 20:47:20 +0100

    kernel/config: add `kernel_stack_size`

--- system/kernel/config.zig
+++ system/kernel/config.zig
@@ -9,8 +9,12 @@ pub const forced_debug_log_scopes = kernel_options.forced_debug_log_scopes;
 // This must be kept in sync with the linker scripts.
 pub const kernel_base_address: core.VirtualAddress = .fromInt(0xffffffff80000000);
 
+/// The size of the usable region of a kernel stack.
+pub const kernel_stack_size = arch.paging.standard_page_size.multiplyScalar(16);
+
 const std = @import("std");
 const core = @import("core");
 const kernel = @import("kernel");
 const builtin = @import("builtin");
 const kernel_options = @import("kernel_options");
+const arch = @import("arch");
