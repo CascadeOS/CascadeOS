c839d2ebcc31611e97c356f155dc72d8ca99850d
Lee Cannon <leecannon@leecannon.xyz>
2024-09-07 19:23:48 +0100

    kernel/acpi: move initialization into init

--- system/kernel/acpi.zig
+++ system/kernel/acpi.zig
@@ -1,70 +1,39 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2024 Lee Cannon <leecannon@leecannon.xyz>
 
-pub const init = struct {
-    /// Get the `n`th matching ACPI table if present.
-    ///
-    /// Uses the `SIGNATURE_STRING: *const [4]u8` decl on the given `T` to find the table.
-    ///
-    /// If the table is not valid, returns `null`.
-    pub fn getTable(comptime T: type, n: usize) ?*const T {
-        var iter = acpi.tableIterator(
-            sdt_header,
-            kernel.directMapFromPhysical,
-        );
+/// Get the `n`th matching ACPI table if present.
+///
+/// Uses the `SIGNATURE_STRING: *const [4]u8` decl on the given `T` to find the table.
+///
+/// If the table is not valid, returns `null`.
+pub fn getTable(comptime T: type, n: usize) ?*const T {
+    var iter = acpi.tableIterator(
+        globals.sdt_header,
+        kernel.directMapFromPhysical,
+    );
 
-        var i: usize = 0;
+    var i: usize = 0;
 
-        while (iter.next()) |header| {
-            if (!header.signatureIs(T.SIGNATURE_STRING)) continue;
+    while (iter.next()) |header| {
+        if (!header.signatureIs(T.SIGNATURE_STRING)) continue;
 
-            if (i != n) continue;
-            i += 1;
+        if (i != n) continue;
+        i += 1;
 
-            if (!header.isValid()) {
-                log.warn("invalid table: {s}", .{header.signatureAsString()});
-                return null;
-            }
-
-            return @ptrCast(header);
+        if (!header.isValid()) {
+            log.warn("invalid table: {s}", .{header.signatureAsString()});
+            return null;
         }
 
-        return null;
+        return @ptrCast(header);
     }
 
-    /// Initialized during `initializeACPITables`.
-    var sdt_header: *const acpi.SharedHeader = undefined;
+    return null;
+}
 
-    pub fn initializeACPITables(rsdp_address: core.VirtualAddress) !void {
-        const rsdp = rsdp_address.toPtr(*const acpi.RSDP);
-
-        log.debug("ACPI revision: {d}", .{rsdp.revision});
-
-        log.debug("validating rsdp", .{});
-        if (!rsdp.isValid()) core.panic("invalid RSDP", null);
-
-        sdt_header = kernel.directMapFromPhysical(rsdp.sdtAddress()).toPtr(*const acpi.SharedHeader);
-
-        log.debug("validating sdt", .{});
-        if (!sdt_header.isValid()) core.panic("invalid SDT", null);
-
-        if (log.levelEnabled(.debug)) {
-            var iter = acpi.tableIterator(
-                sdt_header,
-                kernel.directMapFromPhysical,
-            );
-
-            log.debug("ACPI tables:", .{});
-
-            while (iter.next()) |table| {
-                if (table.isValid()) {
-                    log.debug("  {s}", .{table.signatureAsString()});
-                } else {
-                    log.debug("  {s} - INVALID", .{table.signatureAsString()});
-                }
-            }
-        }
-    }
+pub const globals = struct {
+    /// Initialized during `init.initializeACPITables`.
+    pub var sdt_header: *const acpi.SharedHeader = undefined;
 };
 
 const std = @import("std");
