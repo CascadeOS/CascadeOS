8478d5308e90fa389f9a639ccbd8c614e911d7f1
Lee Cannon <leecannon@leecannon.xyz>
2024-09-17 01:30:31 +0100

    kernel/init: initialize virtual memory

--- system/kernel/kernel.zig
+++ system/kernel/kernel.zig
@@ -6,6 +6,13 @@
 /// Initialized during init and never modified again.
 pub var executors: []Executor = &.{};
 
+/// The core page table.
+///
+/// All other page tables start as a copy of this one.
+///
+/// Initialized during `init.initializeVirtualMemory`.
+pub var core_page_table: arch.paging.PageTable = undefined;
+
 /// The memory layout of the kernel.
 ///
 /// Initialized during `init.buildMemoryLayout`.
@@ -22,3 +29,4 @@ pub const vmm = @import("vmm.zig");
 
 const std = @import("std");
 const core = @import("core");
+const arch = @import("arch");
