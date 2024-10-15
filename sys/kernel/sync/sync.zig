09440ebc94d447f189bf364f5295a88afc426603
Lee Cannon <leecannon@leecannon.xyz>
2024-10-15 21:22:12 +0100

    sys/kernel/sync: implement a `TicketSpinLock`

---
+++ sys/kernel/sync/sync.zig
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2024 Lee Cannon <leecannon@leecannon.xyz>
+
+pub const TicketSpinLock = @import("TicketSpinLock.zig");
+
+const core = @import("core");
+const kernel = @import("kernel");
+const std = @import("std");
