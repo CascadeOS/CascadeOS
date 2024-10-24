53f8624eba4e5a7c882e76feadb0063e4245219d
Lee Cannon <leecannon@leecannon.xyz>
2024-10-24 22:12:00 +0100

    sys/kernel/acpi: implement a rather janky `tryShutdown`
    Eventually we will have a proper ACPI driver but this is good enough for
    now.

--- sys/kernel/acpi.zig
+++ sys/kernel/acpi.zig
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT AND BSD-2-Clause
 // SPDX-FileCopyrightText: 2024 Lee Cannon <leecannon@leecannon.xyz>
+// SPDX-FileCopyrightText: 2019-2024 mintsuki and contributors (https://github.com/lowlevelmemes/acpi-shutdown-hack/blob/115c66d097e9d52015907a4ff27cd1ba34aee0d9/LICENSE)
 
 /// Get the `n`th matching ACPI table if present.
 ///
@@ -29,6 +30,150 @@ pub fn getTable(comptime T: type, n: usize) ?*const T {
     }
 
     return null;
+}
+
+pub fn tryShutdown() !void {
+    // this is ported from https://github.com/lowlevelmemes/acpi-shutdown-hack/blob/trunk/acpi_shutdown_hack.c
+
+    const fadt = getTable(acpi.FADT, 0) orelse return error.FADTNotPresent;
+
+    var s5_addr: [*]const u8 = blk: {
+        const dsdt = kernel.memory_layout
+            .directMapFromPhysical(fadt.getDSDT())
+            .toPtr(*acpi.DSDT);
+        if (!dsdt.header.signatureIs(acpi.DSDT.SIGNATURE_STRING)) return error.DSDTNotPresent;
+
+        const definition_block = dsdt.definitionBlock();
+
+        const s5_offset = std.mem.indexOf(
+            u8,
+            definition_block,
+            "_S5_",
+        ) orelse return error.S5NotPresent;
+
+        break :blk definition_block[s5_offset..].ptr;
+    };
+
+    s5_addr += 4; // skip the last part of NameSeg
+
+    if (s5_addr[0] != 0x12) return error.S5NotPackageOp; // TODO: this code only supports PackageOp
+    s5_addr += 1;
+
+    s5_addr += ((s5_addr[0] & 0xc0) >> 6) + 1; // skip PkgLength
+
+    if (s5_addr[0] < 2) return error.S5NotEnoughElements;
+    s5_addr += 1;
+
+    const SLP_TYPa: u16 = blk: {
+        var value: u64 = 0;
+        s5_addr += parseInteger(s5_addr, &value) orelse return error.FailedToParseSLP_TYPa;
+        break :blk @intCast(value << 10);
+    };
+
+    const SLP_TYPb: u16 = blk: {
+        var value: u64 = 0;
+        s5_addr += parseInteger(s5_addr, &value) orelse return error.FailedToParseSLP_TYPb;
+        break :blk @intCast(value << 10);
+    };
+
+    const SCI_EN: u8 = 1;
+    const SLP_EN = @as(u16, 1) << 13;
+
+    const PM1a_CNT_BLK: u16 = blk: {
+        if (fadt.X_PM1a_CNT_BLK.address != 0) {
+            if (fadt.X_PM1a_CNT_BLK.address_space != .io) return error.X_PM1a_CNT_BLK_NotIO;
+
+            break :blk @intCast(fadt.X_PM1a_CNT_BLK.address);
+        }
+
+        break :blk @intCast(fadt.PM1a_CNT_BLK);
+    };
+
+    if (fadt.SMI_CMD != 0 and fadt.ACPI_ENABLE != 0) {
+        // we have SMM and we need to enable ACPI mode first
+        arch.jank.outb(@intCast(fadt.SMI_CMD), fadt.ACPI_ENABLE);
+
+        for (0..100) |_| _ = arch.jank.inb(0x80);
+
+        while (arch.jank.inw(PM1a_CNT_BLK) & SCI_EN == 0) arch.spinLoopHint();
+    }
+
+    arch.jank.outw(PM1a_CNT_BLK, SLP_TYPa | SLP_EN);
+
+    PM1b_CNT_BLK: {
+        if (fadt.X_PM1b_CNT_BLK.address != 0) {
+            if (fadt.X_PM1b_CNT_BLK.address_space != .io) return error.X_PM1b_CNT_BLK_NotIO;
+
+            arch.jank.outw(@intCast(fadt.X_PM1b_CNT_BLK.address), SLP_TYPb | SLP_EN);
+            break :PM1b_CNT_BLK;
+        }
+
+        if (fadt.PM1b_CNT_BLK != 0) {
+            arch.jank.outw(@intCast(fadt.PM1b_CNT_BLK), SLP_TYPb | SLP_EN);
+        }
+    }
+
+    for (0..100) |_| _ = arch.jank.inb(0x80);
+}
+
+fn parseInteger(s5_addr: [*]const u8, value: *u64) ?u8 {
+    var addr = s5_addr;
+
+    const op = addr[0];
+    addr += 1;
+
+    switch (op) {
+        0x0 => {
+            // ZeroOp
+            value.* = 0;
+            return 1;
+        },
+        0x1 => {
+            // OneOp
+            value.* = 1;
+            return 1;
+        },
+        0xFF => {
+            // OnesOp
+            value.* = ~@as(u64, 0);
+            return 1;
+        },
+        0xA => {
+            // ByteConst
+            value.* = addr[0];
+            return 2; // 1 Type Byte, 1 Data Byte
+        },
+        0xB => {
+            // WordConst
+            value.* =
+                addr[0] |
+                (@as(u16, addr[1]) << 8);
+            return 3; // 1 Type Byte, 2 Data Bytes
+        },
+        0xC => {
+            // DWordConst
+            value.* =
+                addr[0] |
+                (@as(u32, addr[1]) << 8) |
+                (@as(u32, addr[2]) << 16) |
+                (@as(u32, addr[3]) << 24);
+            return 5; // 1 Type Byte, 4 Data Bytes
+        },
+        0xE => {
+            // DWordConst
+            value.* =
+                addr[0] |
+                (@as(u64, addr[1]) << 8) |
+                (@as(u64, addr[2]) << 16) |
+                (@as(u64, addr[3]) << 24) |
+                (@as(u64, addr[4]) << 32) |
+                (@as(u64, addr[5]) << 40) |
+                (@as(u64, addr[6]) << 48) |
+                (@as(u64, addr[7]) << 56);
+            return 9; // 1 Type Byte, 8 Data Bytes
+        },
+        else => return null, // not an integer
+    }
 }
 
 pub const globals = struct {
