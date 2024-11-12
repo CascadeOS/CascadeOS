a647e38a4eb2c0559b6d63a23b4f6f70c981c5e9
Lee Cannon <leecannon@leecannon.xyz>
2024-11-12 22:36:55 +0000

    sys/kernel/sync: implement `Mutex`

--- sys/kernel/sync/sync.zig
+++ sys/kernel/sync/sync.zig
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2024 Lee Cannon <leecannon@leecannon.xyz>
 
+pub const Mutex = @import("Mutex.zig");
 pub const TicketSpinLock = @import("TicketSpinLock.zig");
 pub const WaitQueue = @import("WaitQueue.zig");
 
