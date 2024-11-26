080bfaa6a74c4dbd5c23c602a7172489f974cb69
Lee Cannon <leecannon@leecannon.xyz>
2024-11-26 22:17:16 +0000

    sys/x64/apic: register as a per-executor interrupt time source

--- sys/arch/x64/apic.zig
+++ sys/arch/x64/apic.zig
@@ -32,7 +32,106 @@ pub const init = struct {
         // TODO: task priority
         // TODO: error interrupt
     }
+
+    pub fn registerTimeSource(candidate_time_sources: *init_time.CandidateTimeSources) void {
+        candidate_time_sources.addTimeSource(.{
+            .name = "lapic",
+            .priority = 150,
+            .initialization = if (x64.info.lapic_base_tick_duration_fs != null)
+                .{ .simple = initializeLapicTimer }
+            else
+                .{ .calibration_required = initializeLapicTimerCalibrate },
+            .per_executor_periodic = .{
+                .enableInterruptFn = perExecutorPeriodicEnableInterrupt,
+            },
+        });
+    }
+
+    const divide_configuration: lib_x64.LAPIC.DivideConfigurationRegister = .@"2";
+
+    fn initializeLapicTimer() void {
+        std.debug.assert(x64.info.lapic_base_tick_duration_fs != null);
+
+        tick_duration_fs = x64.info.lapic_base_tick_duration_fs.? * divide_configuration.toInt();
+        log.debug("tick duration (fs) from cpuid: {}", .{tick_duration_fs});
+    }
+
+    fn initializeLapicTimerCalibrate(
+        reference_counter: init_time.ReferenceCounter,
+    ) void {
+        lapic.writeDivideConfigurationRegister(divide_configuration);
+
+        lapic.writeLVTTimerRegister(.{
+            .vector = .debug, // interrupt is masked so it doesnt matter what the vector is set to
+            .timer_mode = .oneshot,
+            .masked = true,
+        });
+
+        // warmup
+        {
+            const warmup_duration = core.Duration.from(1, .millisecond);
+            const number_of_warmups = 5;
+
+            var total_warmup_ticks: u64 = 0;
+
+            for (0..number_of_warmups) |_| {
+                reference_counter.prepareToWaitFor(warmup_duration);
+
+                lapic.writeInitialCountRegister(std.math.maxInt(u32));
+                reference_counter.waitFor(warmup_duration);
+                const end = lapic.readCurrentCountRegister();
+                lapic.writeInitialCountRegister(0);
+
+                total_warmup_ticks += std.math.maxInt(u32) - end;
+            }
+
+            std.mem.doNotOptimizeAway(&total_warmup_ticks);
+        }
+
+        const sample_duration = core.Duration.from(5, .millisecond);
+        const number_of_samples = 5;
+        var total_ticks: u64 = 0;
+
+        for (0..number_of_samples) |_| {
+            reference_counter.prepareToWaitFor(sample_duration);
+
+            lapic.writeInitialCountRegister(std.math.maxInt(u32));
+            reference_counter.waitFor(sample_duration);
+            const end = lapic.readCurrentCountRegister();
+            lapic.writeInitialCountRegister(0);
+
+            total_ticks += std.math.maxInt(u32) - end;
+        }
+
+        const average_ticks = total_ticks / number_of_samples;
+
+        tick_duration_fs = (sample_duration.value * kernel.time.fs_per_ns) / average_ticks;
+        log.debug("tick duration (fs) using reference counter: {}", .{tick_duration_fs});
+    }
+
+    fn perExecutorPeriodicEnableInterrupt(period: core.Duration) void {
+        lapic.writeInitialCountRegister(0);
+        lapic.writeDivideConfigurationRegister(divide_configuration);
+
+        lapic.writeLVTTimerRegister(.{
+            .vector = x64.interrupts.Interrupt.per_executor_periodic.toInterruptVector(),
+            .timer_mode = .periodic,
+            .masked = false,
+        });
+
+        const ticks = std.math.cast(
+            u32,
+            (period.value * kernel.time.fs_per_ns) / tick_duration_fs,
+        ) orelse core.panic("period is too long", null);
+
+        lapic.writeInitialCountRegister(ticks);
+    }
 };
+
+/// The duration of a tick in femptoseconds.
+///
+/// Initalized in `init.initializeLapicTimer[Calibrate]`
+var tick_duration_fs: u64 = undefined;
 
 /// Initialized in `init.captureApicInformation`.
 var lapic: lib_x64.LAPIC = .{
@@ -49,3 +148,4 @@ const log = kernel.log.scoped(.apic);
 const arch = @import("arch");
 const acpi = @import("acpi");
 const boot = @import("boot");
+const init_time = @import("init").time;
