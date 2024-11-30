d0fc2bced1e380f565cb6175eb80aa6cb0d3582a
Lee Cannon <leecannon@leecannon.xyz>
2024-11-30 01:03:37 +0000

    sys/boot: restructure boot api

--- sys/boot/boot.zig
+++ sys/boot/boot.zig
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2024 Lee Cannon <leecannon@leecannon.xyz>
 
-/// Exports bootloader entry points.
+/// Exports bootloader entry points and any other required exported symbols.
 ///
 /// Required to be called at comptime from the kernels root file 'system/root.zig'.
 pub fn exportEntryPoints() void {
@@ -9,9 +9,16 @@ pub fn exportEntryPoints() void {
         // export a fallback entry point for unknown bootloaders
         @export(&arch.init.unknownBootloaderEntryPoint, .{ .name = "_start" });
 
-        // ensure the limine requests are exported
-        _ = &limine_requests;
+        limine.exportRequests();
     }
+}
+
+/// Returns the kernel virtual and physical base addresses provided by the bootloader, if any.
+pub fn kernelBaseAddress() ?KernelBaseAddress {
+    return switch (bootloader_api) {
+        .limine => limine.kernelBaseAddress(),
+        .unknown => null,
+    };
 }
 
 pub const KernelBaseAddress = struct {
@@ -19,319 +26,194 @@ pub const KernelBaseAddress = struct {
     physical: core.PhysicalAddress,
 };
 
-/// Returns the kernel virtual and physical base addresses provided by the bootloader, if any.
-pub fn kernelBaseAddress() ?KernelBaseAddress {
-    switch (bootloader_api) {
-        .limine => if (limine_requests.kernel_address.response) |resp| {
-            return .{
-                .virtual = resp.virtual_base,
-                .physical = resp.physical_base,
-            };
-        },
-        .unknown => {},
-    }
-
-    return null;
-}
-
 /// Returns the direct map address provided by the bootloader, if any.
 pub fn directMapAddress() ?core.VirtualAddress {
-    switch (bootloader_api) {
-        .limine => if (limine_requests.hhdm.response) |resp| {
-            return resp.offset;
-        },
-        .unknown => {},
-    }
-
-    return null;
-}
-
-/// Returns an iterator over the memory map entries, iterating in the given direction.
-pub fn memoryMap(direction: core.Direction) ?MemoryMapEntry.Iterator {
-    switch (bootloader_api) {
-        .limine => if (limine_requests.memmap.response) |resp| {
-            const entries = resp.entries();
-            return .{
-                .limine = .{
-                    .index = switch (direction) {
-                        .forward => 0,
-                        .backward => entries.len,
-                    },
-                    .entries = entries,
-                    .direction = direction,
-                },
-            };
-        },
-        .unknown => {},
-    }
-
-    return null;
-}
-
-/// An entry in the memory map provided by the bootloader.
-pub const MemoryMapEntry = struct {
-    range: core.PhysicalRange,
-    type: Type,
-
-    pub const Type = enum {
-        free,
-        in_use,
-        reserved,
-        bootloader_reclaimable,
-        acpi_reclaimable,
-        unusable,
-
-        unknown,
+    return switch (bootloader_api) {
+        .limine => limine.directMapAddress(),
+        .unknown => null,
     };
-
-    pub fn print(entry: MemoryMapEntry, writer: std.io.AnyWriter, indent: usize) !void {
-        try writer.writeAll("MemoryMapEntry - ");
-
-        try writer.writeAll(@tagName(entry.type));
-
-        try writer.writeAll(" - ");
-
-        try entry.range.print(writer, indent);
-    }
-
-    pub inline fn format(
-        value: MemoryMapEntry,
-        comptime fmt: []const u8,
-        options: std.fmt.FormatOptions,
-        writer: anytype,
-    ) !void {
-        _ = options;
-        _ = fmt;
-        return if (@TypeOf(writer) == std.io.AnyWriter)
-            print(value, writer, 0)
-        else
-            print(value, writer.any(), 0);
-    }
-
-    fn __helpZls() void {
-        MemoryMapEntry.print(undefined, @as(std.fs.File.Writer, undefined), 0);
-    }
-
-    /// An iterator over the memory map entries provided by the bootloader.
-    pub const Iterator = union(enum) {
-        limine: LimineMemoryMapIterator,
-
-        /// Returns the next memory map entry from the iterator, if any remain.
-        pub fn next(self: *Iterator) ?MemoryMapEntry {
-            while (true) {
-                const opt_entry = switch (self.*) {
-                    inline else => |*i| i.next(),
-                };
-
-                if (opt_entry) |entry| {
-                    if (entry.range.address.equal(core.PhysicalAddress.fromInt(0x000000fd00000000))) {
-                        // this is a qemu specific hack to not have a 1TiB direct map
-                        // this `0xfd00000000` memory region is not listed in qemu's `info mtree` but the bootloader reports it
-                        continue;
-                    }
-                }
-
-                return opt_entry;
-            }
-        }
-
-        const LimineMemoryMapIterator = struct {
-            index: usize,
-            entries: []const *const limine.Memmap.Entry,
-            direction: core.Direction,
-
-            pub fn next(self: *LimineMemoryMapIterator) ?MemoryMapEntry {
-                const limine_entry = switch (self.direction) {
-                    .backward => blk: {
-                        if (self.index == 0) return null;
-                        self.index -= 1;
-                        break :blk self.entries[self.index];
-                    },
-                    .forward => blk: {
-                        if (self.index >= self.entries.len) return null;
-                        const entry = self.entries[self.index];
-                        self.index += 1;
-                        break :blk entry;
-                    },
-                };
-
-                return .{
-                    .range = .fromAddr(limine_entry.base, limine_entry.length),
-                    .type = switch (limine_entry.type) {
-                        .usable => .free,
-                        .kernel_and_modules, .framebuffer => .in_use,
-                        .reserved, .acpi_nvs => .reserved,
-                        .bootloader_reclaimable => .bootloader_reclaimable,
-                        .acpi_reclaimable => .acpi_reclaimable,
-                        .bad_memory => .unusable,
-                        else => .unknown,
-                    },
-                };
-            }
-        };
-    };
-};
+}
 
 /// Returns the ACPI RSDP address provided by the bootloader, if any.
 pub fn rsdp() ?core.Address {
-    switch (bootloader_api) {
-        .limine => if (limine_requests.rsdp.response) |resp| {
-            return resp.address(limine_requests.limine_revison);
-        },
-        .unknown => {},
-    }
-
-    return null;
-}
-
-pub fn cpuDescriptors() ?CpuDescriptor.Iterator {
-    switch (bootloader_api) {
-        .limine => if (limine_requests.smp.response) |resp| {
-            const entries = resp.cpus();
-            return .{
-                .limine = .{
-                    .index = 0,
-                    .entries = entries,
-                },
-            };
-        },
-        .unknown => {},
-    }
-
-    return null;
-}
-
-pub const CpuDescriptor = struct {
-    _raw: Raw,
-
-    pub fn boot(
-        self: CpuDescriptor,
-        user_data: *anyopaque,
-        comptime targetFn: fn (user_data: *anyopaque) noreturn,
-    ) void {
-        switch (self._raw) {
-            .limine => |limine_info| {
-                const trampolineFn = struct {
-                    fn trampolineFn(smp_info: *const limine.SMP.Response.SMPInfo) callconv(.C) noreturn {
-                        targetFn(@ptrFromInt(smp_info.extra_argument));
-                    }
-                }.trampolineFn;
-
-                @atomicStore(
-                    usize,
-                    &limine_info.extra_argument,
-                    @intFromPtr(user_data),
-                    .release,
-                );
-
-                @atomicStore(
-                    ?*const fn (*const limine.SMP.Response.SMPInfo) callconv(.C) noreturn,
-                    &limine_info.goto_address,
-                    &trampolineFn,
-                    .release,
-                );
-            },
-        }
-    }
-
-    pub fn processorId(self: CpuDescriptor) u32 {
-        return switch (self._raw) {
-            .limine => |limine_info| limine_info.processor_id,
-        };
-    }
-
-    pub const Raw = union(enum) {
-        limine: *limine.SMP.Response.SMPInfo,
+    return switch (bootloader_api) {
+        .limine => limine.rsdp(),
+        .unknown => null,
     };
-
-    /// An iterator over the cpu descriptors provided by the bootloader.
-    pub const Iterator = union(enum) {
-        limine: LimineCpuDescriptorIterator,
-
-        pub fn count(self: Iterator) usize {
-            return switch (self) {
-                inline else => |i| i.count(),
-            };
-        }
-
-        /// Returns the next cpu descriptor from the iterator, if any remain.
-        pub fn next(self: *Iterator) ?CpuDescriptor {
-            return switch (self.*) {
-                inline else => |*i| i.next(),
-            };
-        }
-
-        const LimineCpuDescriptorIterator = struct {
-            index: usize,
-            entries: []*limine.SMP.Response.SMPInfo,
-
-            pub fn count(self: LimineCpuDescriptorIterator) usize {
-                return self.entries.len;
-            }
-
-            pub fn next(self: *LimineCpuDescriptorIterator) ?CpuDescriptor {
-                if (self.index >= self.entries.len) return null;
-
-                const smp_info = self.entries[self.index];
-
-                self.index += 1;
-
-                return .{
-                    ._raw = .{ .limine = smp_info },
-                };
-            }
-        };
-    };
-};
+}
 
 pub fn x2apicEnabled() bool {
-    if (@import("cascade_target").arch != .x64) @compileError("x2apicEnabled can only be called on x64");
+    if (@import("cascade_target").arch != .x64) {
+        @compileError("x2apicEnabled can only be called on x64");
+    }
 
-    switch (bootloader_api) {
-        .limine => {
-            const smp_response = limine_requests.smp.response orelse return false;
-            return smp_response.flags.x2apic_enabled;
-        },
+    return switch (bootloader_api) {
+        .limine => limine.x2apicEnabled(),
         .unknown => return false,
-    }
-}
-
-fn limineEntryPoint() callconv(.C) noreturn {
-    bootloader_api = .limine;
-    if (limine_requests.limine_base_revison.revison == .@"0") {
-        // limine sets the `revison` field to `0` to signal that the requested revision is supported
-        limine_requests.limine_revison = limine_requests.target_limine_revison;
-    }
-
-    @call(.never_inline, @import("root").initEntryPoint, .{}) catch |err| {
-        core.panicFmt("unhandled error: {s}", .{@errorName(err)}, @errorReturnTrace());
     };
-    core.panic("`init.initStage1` returned", null);
 }
 
-const limine_requests = struct {
-    // TODO: update to 3, needs annoying changes as things like the ACPI RSDP are not mapped in the
-    //       HHDM from that revision onwards
-    const target_limine_revison: limine.BaseRevison.Revison = .@"2";
-    var limine_revison: limine.BaseRevison.Revison = .@"0";
+/// Returns an iterator over the memory map entries, iterating in the given direction.
+pub fn memoryMap(direction: core.Direction) ?MemoryMap {
+    return switch (bootloader_api) {
+        .limine => limine.memoryMap(direction),
+        .unknown => null,
+    };
+}
 
-    export var limine_base_revison: limine.BaseRevison = .{ .revison = target_limine_revison };
-    export var entry_point: limine.EntryPoint = .{ .entry = limineEntryPoint };
-    export var kernel_address: limine.KernelAddress = .{};
-    export var hhdm: limine.HHDM = .{};
-    export var memmap: limine.Memmap = .{};
-    export var rsdp: limine.RSDP = .{};
-    export var smp: limine.SMP = .{ .flags = .{ .x2apic = true } };
+pub const MemoryMap = struct {
+    backing: [backing_size]u8 align(backing_align),
+
+    pub fn next(self: *MemoryMap) ?Entry {
+        while (true) {
+            const entry = switch (bootloader_api) {
+                .limine => limine.MemoryMapIterator.next(self),
+                .unknown => null,
+            } orelse
+                return null;
+
+            if (entry.range.address.equal(.fromInt(0xfd00000000))) {
+                // this is a qemu specific hack to not have a 1TiB direct map
+                // this `0xfd00000000` memory region is not listed in qemu's `info mtree` but the bootloader reports it
+                continue;
+            }
+
+            return entry;
+        }
+    }
+
+    /// An entry in the memory map provided by the bootloader.
+    pub const Entry = struct {
+        range: core.PhysicalRange,
+        type: Type,
+
+        pub const Type = enum {
+            free,
+            in_use,
+            reserved,
+            bootloader_reclaimable,
+            acpi_reclaimable,
+            unusable,
+
+            unknown,
+        };
+
+        pub fn print(entry: Entry, writer: std.io.AnyWriter, indent: usize) !void {
+            try writer.writeAll("Entry - ");
+
+            try writer.writeAll(@tagName(entry.type));
+
+            try writer.writeAll(" - ");
+
+            try entry.range.print(writer, indent);
+        }
+
+        pub inline fn format(
+            value: Entry,
+            comptime fmt: []const u8,
+            options: std.fmt.FormatOptions,
+            writer: anytype,
+        ) !void {
+            _ = options;
+            _ = fmt;
+            return if (@TypeOf(writer) == std.io.AnyWriter)
+                print(value, writer, 0)
+            else
+                print(value, writer.any(), 0);
+        }
+
+        fn __helpZls() void {
+            Entry.print(undefined, @as(std.fs.File.Writer, undefined), 0);
+        }
+    };
+
+    const backing_size: usize = @max(
+        @sizeOf(limine.MemoryMapIterator),
+        0,
+    );
+
+    const backing_align: usize = @max(
+        @alignOf(limine.MemoryMapIterator),
+        0,
+    );
 };
 
-var bootloader_api: BootloaderAPI = .unknown;
+pub fn cpuDescriptors() ?CpuDescriptors {
+    return switch (bootloader_api) {
+        .limine => limine.cpuDescriptors(),
+        .unknown => null,
+    };
+}
 
-const BootloaderAPI = enum {
+pub const CpuDescriptors = struct {
+    backing: [descriptors_backing_size]u8 align(descriptors_backing_align),
+
+    pub fn count(self: *const CpuDescriptors) usize {
+        return switch (bootloader_api) {
+            .limine => limine.CpuDescriptorIterator.count(self),
+            .unknown => 0,
+        };
+    }
+
+    /// Returns the next cpu descriptor from the iterator, if any remain.
+    pub fn next(self: *CpuDescriptors) ?Descriptor {
+        return switch (bootloader_api) {
+            .limine => limine.CpuDescriptorIterator.next(self),
+            .unknown => null,
+        };
+    }
+
+    pub const Descriptor = struct {
+        backing: [descriptor_backing_size]u8 align(descriptor_backing_align),
+
+        pub fn boot(
+            self: *const Descriptor,
+            user_data: *anyopaque,
+            target_fn: fn (user_data: *anyopaque) noreturn,
+        ) void {
+            switch (bootloader_api) {
+                .limine => limine.CpuDescriptorIterator.bootFn(self, user_data, target_fn),
+                .unknown => unreachable,
+            }
+        }
+
+        pub fn processorId(self: *const Descriptor) u32 {
+            return switch (bootloader_api) {
+                .limine => limine.CpuDescriptorIterator.processorId(self),
+                .unknown => unreachable,
+            };
+        }
+
+        const descriptor_backing_size: usize = @max(
+            @sizeOf(limine.CpuDescriptorIterator.Descriptor),
+            0,
+        );
+
+        const descriptor_backing_align: usize = @max(
+            @alignOf(limine.CpuDescriptorIterator.Descriptor),
+            0,
+        );
+    };
+
+    const descriptors_backing_size: usize = @max(
+        @sizeOf(limine.CpuDescriptorIterator),
+        0,
+    );
+
+    const descriptors_backing_align: usize = @max(
+        @alignOf(limine.CpuDescriptorIterator),
+        0,
+    );
+};
+
+pub var bootloader_api: BootloaderAPI = .unknown;
+
+pub const BootloaderAPI = enum {
     unknown,
     limine,
 };
 
 const std = @import("std");
 const core = @import("core");
-const limine = @import("limine");
 const arch = @import("arch");
+const limine = @import("limine.zig");
