dae0448899a43d1759484d2b783d0ef91705dbd5
Lee Cannon <leecannon@leecannon.xyz>
2024-12-15 21:14:18 +0000

    sys/kernel/mem: move `MapType` to its own file

---
+++ sys/kernel/mem/MapType.zig
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2024 Lee Cannon <leecannon@leecannon.xyz>
+
+const MapType = @This();
+
+/// Accessible from userspace.
+user: bool = false,
+
+/// A global mapping that is not flushed on context switch.
+global: bool = false,
+
+/// Writeable.
+writeable: bool = false,
+
+/// Executable.
+executable: bool = false,
+
+/// Uncached.
+no_cache: bool = false,
+
+pub fn equal(a: MapType, b: MapType) bool {
+    return a.user == b.user and
+        a.global == b.global and
+        a.writeable == b.writeable and
+        a.executable == b.executable and
+        a.no_cache == b.no_cache;
+}
+
+pub fn print(value: MapType, writer: std.io.AnyWriter, indent: usize) !void {
+    _ = indent;
+
+    try writer.writeAll("Type{ ");
+
+    const buffer: []const u8 = &[_]u8{
+        if (value.user) 'U' else 'K',
+        if (value.writeable) 'W' else 'R',
+        if (value.executable) 'X' else '-',
+        if (value.global) 'G' else '-',
+        if (value.no_cache) 'C' else '-',
+    };
+
+    try writer.writeAll(buffer);
+    try writer.writeAll(" }");
+}
+
+pub inline fn format(
+    region: MapType,
+    comptime fmt: []const u8,
+    options: std.fmt.FormatOptions,
+    writer: anytype,
+) !void {
+    _ = options;
+    _ = fmt;
+    return if (@TypeOf(writer) == std.io.AnyWriter)
+        print(region, writer, 0)
+    else
+        print(region, writer.any(), 0);
+}
+
+fn __helpZls() void {
+    MapType.print(undefined, @as(std.fs.File.Writer, undefined), 0);
+}
+
+const core = @import("core");
+const kernel = @import("kernel");
+const std = @import("std");
+const arch = @import("arch");
