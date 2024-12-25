85d4a7a5f962eb7bbd9b1d65d91e036bd7863e11
Lee Cannon <leecannon@leecannon.xyz>
2024-12-25 23:18:21 +0000

    kernel/boot: provide `memmap`

--- kernel/boot/boot.zig
+++ kernel/boot/boot.zig
@@ -14,6 +14,91 @@ pub const KernelBaseAddress = struct {
     physical: core.PhysicalAddress,
 };
 
+/// Returns an iterator over the memory map entries, iterating in the given direction.
+pub fn memoryMap(direction: core.Direction) ?MemoryMap {
+    return switch (bootloader_api) {
+        .limine => limine.memoryMap(direction),
+        .unknown => null,
+    };
+}
+
+pub const MemoryMap = struct {
+    backing: [backing_size]u8 align(backing_align),
+
+    pub fn next(self: *MemoryMap) ?Entry {
+        while (true) {
+            const entry = switch (bootloader_api) {
+                .limine => limine.MemoryMapIterator.next(self),
+                .unknown => null,
+            } orelse
+                return null;
+
+            if (entry.range.address.equal(.fromInt(0xfd00000000))) {
+                // this is a qemu specific hack to not have a 1TiB direct map
+                // this `0xfd00000000` memory region is not listed in qemu's `info mtree` but the bootloader reports it
+                continue;
+            }
+
+            return entry;
+        }
+    }
+
+    /// An entry in the memory map provided by the bootloader.
+    pub const Entry = struct {
+        range: core.PhysicalRange,
+        type: Type,
+
+        pub const Type = enum {
+            free,
+            in_use,
+            reserved,
+            bootloader_reclaimable,
+            acpi_reclaimable,
+            unusable,
+
+            unknown,
+        };
+
+        pub fn print(entry: Entry, writer: std.io.AnyWriter, indent: usize) !void {
+            try writer.writeAll("Entry - ");
+
+            try writer.writeAll(@tagName(entry.type));
+
+            try writer.writeAll(" - ");
+
+            try entry.range.print(writer, indent);
+        }
+
+        pub inline fn format(
+            value: Entry,
+            comptime fmt: []const u8,
+            options: std.fmt.FormatOptions,
+            writer: anytype,
+        ) !void {
+            _ = options;
+            _ = fmt;
+            return if (@TypeOf(writer) == std.io.AnyWriter)
+                print(value, writer, 0)
+            else
+                print(value, writer.any(), 0);
+        }
+
+        fn __helpZls() void {
+            Entry.print(undefined, @as(std.fs.File.Writer, undefined), 0);
+        }
+    };
+
+    const backing_size: usize = @max(
+        @sizeOf(limine.MemoryMapIterator),
+        0,
+    );
+
+    const backing_align: usize = @max(
+        @alignOf(limine.MemoryMapIterator),
+        0,
+    );
+};
+
 /// Exports bootloader entry points and any other required exported symbols.
 ///
 /// Required to be called at comptime from the kernels root file 'kernel/kernel.zig'.
--- kernel/boot/limine.zig
+++ kernel/boot/limine.zig
@@ -11,6 +11,67 @@ pub fn kernelBaseAddress() ?boot.KernelBaseAddress {
     };
 }
 
+pub fn memoryMap(direction: core.Direction) ?boot.MemoryMap {
+    const resp = requests.memmap.response orelse
+        return null;
+
+    var result: boot.MemoryMap = .{
+        .backing = undefined,
+    };
+
+    const limine_memory_map = std.mem.bytesAsValue(MemoryMapIterator, &result.backing);
+
+    const entries = resp.entries();
+
+    limine_memory_map.* = .{
+        .index = switch (direction) {
+            .forward => 0,
+            .backward => entries.len,
+        },
+        .entries = entries,
+        .direction = direction,
+    };
+
+    return result;
+}
+
+pub const MemoryMapIterator = struct {
+    index: usize,
+    entries: []const *const limine.Memmap.Entry,
+    direction: core.Direction,
+
+    pub fn next(memory_map: *boot.MemoryMap) ?boot.MemoryMap.Entry {
+        const self: *MemoryMapIterator = @ptrCast(@alignCast(&memory_map.backing));
+
+        const limine_entry = switch (self.direction) {
+            .backward => blk: {
+                if (self.index == 0) return null;
+                self.index -= 1;
+                break :blk self.entries[self.index];
+            },
+            .forward => blk: {
+                if (self.index >= self.entries.len) return null;
+                const entry = self.entries[self.index];
+                self.index += 1;
+                break :blk entry;
+            },
+        };
+
+        return .{
+            .range = .fromAddr(limine_entry.base, limine_entry.length),
+            .type = switch (limine_entry.type) {
+                .usable => .free,
+                .executable_and_modules, .framebuffer => .in_use,
+                .reserved, .acpi_nvs => .reserved,
+                .bootloader_reclaimable => .bootloader_reclaimable,
+                .acpi_reclaimable => .acpi_reclaimable,
+                .bad_memory => .unusable,
+                else => .unknown,
+            },
+        };
+    }
+};
+
 fn limineEntryPoint() callconv(.C) noreturn {
     kernel.boot.bootloader_api = .limine;
 
@@ -42,6 +103,7 @@ const requests = struct {
     var limine_base_revison: limine.BaseRevison = .{ .revison = target_limine_revison };
     var entry_point: limine.EntryPoint = .{ .entry = limineEntryPoint };
     var kernel_address: limine.ExecutableAddress = .{};
+    var memmap: limine.Memmap = .{};
 };
 
 const std = @import("std");
