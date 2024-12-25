f39d9f7a9ee867c8e94558cdb1af59ee88fc9639
Lee Cannon <leecannon@leecannon.xyz>
2024-12-25 22:25:52 +0000

    kernel/debug: directly reference the `std.debug.FormattedPanic` implementation

--- kernel/debug.zig
+++ kernel/debug.zig
@@ -1,57 +1,31 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2024 Lee Cannon <leecannon@leecannon.xyz>
 
-/// Entry point from the Zig language upon a panic.
-fn zigPanic(
-    msg: []const u8,
-    error_return_trace: ?*const std.builtin.StackTrace,
-    return_address_opt: ?usize,
-) noreturn {
-    @branchHint(.cold);
-
-    _ = return_address_opt;
-    _ = error_return_trace;
-    _ = msg;
-
-    while (true) {
-        kernel.arch.interrupts.disableInterruptsAndHalt();
-    }
-}
-
 /// Zig panic interface.
 pub const Panic = struct {
-    pub const call = zigPanic;
-
-    pub fn sentinelMismatch(expected: anytype, found: @TypeOf(expected)) noreturn {
+    /// Entry point from the Zig language upon a panic.
+    pub fn call(
+        msg: []const u8,
+        error_return_trace: ?*const std.builtin.StackTrace,
+        return_address_opt: ?usize,
+    ) noreturn {
         @branchHint(.cold);
-        std.debug.panicExtra(null, @returnAddress(), "sentinel mismatch: expected {any}, found {any}", .{
-            expected, found,
-        });
+
+        _ = return_address_opt;
+        _ = error_return_trace;
+        _ = msg;
+
+        while (true) {
+            kernel.arch.interrupts.disableInterruptsAndHalt();
+        }
     }
 
-    pub fn unwrapError(error_return_trace: ?*std.builtin.StackTrace, err: anyerror) noreturn {
-        @branchHint(.cold);
-        std.debug.panicExtra(error_return_trace, @returnAddress(), "attempt to unwrap error: {s}", .{@errorName(err)});
-    }
-
-    pub fn outOfBounds(index: usize, len: usize) noreturn {
-        @branchHint(.cold);
-        std.debug.panicExtra(null, @returnAddress(), "index out of bounds: index {d}, len {d}", .{ index, len });
-    }
-
-    pub fn startGreaterThanEnd(start: usize, end: usize) noreturn {
-        @branchHint(.cold);
-        std.debug.panicExtra(null, @returnAddress(), "start index {d} is larger than end index {d}", .{ start, end });
-    }
-
-    pub fn inactiveUnionField(active: anytype, accessed: @TypeOf(active)) noreturn {
-        @branchHint(.cold);
-        std.debug.panicExtra(null, @returnAddress(), "access of union field '{s}' while field '{s}' is active", .{
-            @tagName(accessed), @tagName(active),
-        });
-    }
-
-    pub const messages = std.debug.SimplePanic.messages;
+    pub const sentinelMismatch = std.debug.FormattedPanic.sentinelMismatch;
+    pub const unwrapError = std.debug.FormattedPanic.unwrapError;
+    pub const outOfBounds = std.debug.FormattedPanic.outOfBounds;
+    pub const startGreaterThanEnd = std.debug.FormattedPanic.startGreaterThanEnd;
+    pub const inactiveUnionField = std.debug.FormattedPanic.inactiveUnionField;
+    pub const messages = std.debug.FormattedPanic.messages;
 };
 
 const std = @import("std");
