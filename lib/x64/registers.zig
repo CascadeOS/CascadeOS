5b6dab5aeefcff00a8e7b610a18bdbcb353c29a5
Lee Cannon <leecannon@leecannon.xyz>
2024-12-29 21:54:40 +0000

    lib/x64: add doc-comments to `Cr0`

--- lib/x64/registers.zig
+++ lib/x64/registers.zig
@@ -174,32 +174,43 @@ pub const RFlags = packed struct(u64) {
 };
 
 pub const Cr0 = packed struct(u64) {
+    /// Enables protected mode.
     protected_mode_enable: bool,
 
+    /// Enables monitoring of the coprocessor.
     monitor_coprocessor: bool,
 
+    /// Force all x87 and MMX instructions to cause an `#NE` exception.
     emulate_coprocessor: bool,
 
+    /// Automatically set to 1 on _hardware_ task switch.
     task_switched: bool,
 
+    /// Indicates support of 387DX math coprocessor instructions.
     extension_type: bool,
 
+    /// Enables the native (internal) error reporting mechanism for x87 FPU errors.
     numeric_error: bool,
 
     _reserved6_15: u10,
 
+    /// Controls whether supervisor-level writes to read-only pages are inhibited.
     write_protect: bool,
 
     _reserved17: u1,
 
+    /// Enables automatic usermode alignment checking if `RFlags.alignment_mask` is also set.
     alignment_mask: bool,
 
     _reserved19_28: u10,
 
+    /// Ignored, should always be unset.
     not_write_through: bool,
 
+    /// Disables some processor caches, specifics are model-dependent.
     cache_disable: bool,
 
+    /// Enables paging.
     paging: bool,
 
     _reserved32_63: u32,
