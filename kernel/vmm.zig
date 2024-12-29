75a94cab21b4d9c868a11a482ac301065c4228de
Lee Cannon <leecannon@leecannon.xyz>
2024-12-29 20:23:49 +0000

    kernel/vmm: add various HHDM translation functions

--- kernel/vmm.zig
+++ kernel/vmm.zig
@@ -1,9 +1,50 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2024 Lee Cannon <leecannon@leecannon.xyz>
 
+//! Virtual memory management.
+
 /// Returns the virtual address corresponding to this physical address in the direct map.
 pub fn directMapFromPhysical(self: core.PhysicalAddress) core.VirtualAddress {
     return .{ .value = self.value + globals.direct_map.address.value };
+}
+
+/// Returns the virtual address corresponding to this physical address in the non-cached direct map.
+pub fn nonCachedDirectMapFromPhysical(self: core.PhysicalAddress) core.VirtualAddress {
+    return .{ .value = self.value + globals.non_cached_direct_map.address.value };
+}
+
+/// Returns a virtual range corresponding to this physical range in the direct map.
+pub fn directMapFromPhysicalRange(self: core.PhysicalRange) core.VirtualRange {
+    return .{
+        .address = directMapFromPhysical(self.address),
+        .size = self.size,
+    };
+}
+
+/// Returns the physical address of the given virtual address if it is in the direct map.
+pub fn physicalFromDirectMap(self: core.VirtualAddress) error{AddressNotInDirectMap}!core.PhysicalAddress {
+    if (globals.direct_map.contains(self)) {
+        return .{ .value = self.value -% globals.direct_map.address.value };
+    }
+    return error.AddressNotInDirectMap;
+}
+
+/// Returns the physical range of the given direct map virtual range.
+pub fn physicalRangeFromDirectMap(self: core.VirtualRange) error{AddressNotInDirectMap}!core.PhysicalRange {
+    if (globals.direct_map.containsRange(self)) {
+        return .{
+            .address = .fromInt(self.address.value -% globals.direct_map.address.value),
+            .size = self.size,
+        };
+    }
+    return error.AddressNotInDirectMap;
+}
+
+/// Returns the physical address of the given kernel ELF section virtual address.
+///
+/// It is the caller's responsibility to ensure that the given virtual address is in the kernel ELF sections.
+pub fn physicalFromKernelSectionUnsafe(self: core.VirtualAddress) core.PhysicalAddress {
+    return .{ .value = self.value -% globals.physical_to_virtual_offset.value };
 }
 
 pub const globals = struct {
