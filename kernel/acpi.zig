e04987ff3e06b743bf56b5a9e810f17751ac2ae7
Lee Cannon <leecannon@leecannon.xyz>
2024-12-30 22:17:26 +0000

    kernel/acpi: implement `getTable`

--- kernel/acpi.zig
+++ kernel/acpi.zig
@@ -2,6 +2,40 @@
 // SPDX-FileCopyrightText: 2024 Lee Cannon <leecannon@leecannon.xyz>
 // SPDX-FileCopyrightText: 2019-2024 mintsuki and contributors (https://github.com/lowlevelmemes/acpi-shutdown-hack/blob/115c66d097e9d52015907a4ff27cd1ba34aee0d9/LICENSE)
 
+/// Get the `n`th matching ACPI table if present.
+///
+/// Uses the `SIGNATURE_STRING: *const [4]u8` decl on the given `T` to find the table.
+///
+/// If the table is not valid, returns `null`.
+pub fn getTable(comptime T: type, n: usize) ?*const T {
+    var iter = acpi.tableIterator(
+        globals.sdt_header,
+        kernel.vmm.nonCachedDirectMapFromPhysical,
+    );
+
+    var i: usize = 0;
+
+    while (iter.next()) |header| {
+        if (!header.signatureIs(T.SIGNATURE_STRING)) {
+            continue;
+        }
+
+        if (i != n) {
+            i += 1;
+            continue;
+        }
+
+        if (!header.isValid()) {
+            log.warn("invalid table: {s}", .{header.signatureAsString()});
+            return null;
+        }
+
+        return @ptrCast(header);
+    }
+
+    return null;
+}
+
 const globals = struct {
     /// Initialized during `init.initializeACPITables`.
     var sdt_header: *const acpi.SharedHeader = undefined;
@@ -48,3 +82,4 @@ const std = @import("std");
 const core = @import("core");
 const kernel = @import("kernel");
 const acpi = @import("acpi");
+const log = kernel.log.scoped(.acpi);
