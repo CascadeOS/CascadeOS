7765f507f01db8fd36530d5e310f5ba81e7777e8
Lee Cannon <leecannon@leecannon.xyz>
2024-12-30 21:45:06 +0000

    kernel/x64/paging: implemement `mapToPhysicalRangeAllPageSizes`

--- kernel/arch/x64/paging.zig
+++ kernel/arch/x64/paging.zig
@@ -18,6 +18,99 @@ pub fn loadPageTable(physical_address: core.PhysicalAddress) void {
     lib_x64.registers.Cr3.writeAddress(physical_address);
 }
 
+fn applyMapType(map_type: MapType, entry: *PageTable.Entry) void {
+    if (map_type.user) entry.user_accessible.write(true);
+
+    if (map_type.global) entry.global.write(true);
+
+    if (x64.info.cpu_id.execute_disable) {
+        @branchHint(.likely); // modern CPUs support NX
+
+        if (!map_type.executable) entry.no_execute.write(true);
+    }
+
+    if (map_type.writeable) entry.writeable.write(true);
+
+    if (map_type.no_cache) {
+        entry.no_cache.write(true);
+        entry.write_through.write(true);
+    }
+}
+
+fn applyParentMapType(map_type: MapType, entry: *PageTable.Entry) void {
+    entry.writeable.write(true);
+    if (map_type.user) entry.user_accessible.write(true);
+}
+
+fn ensureNextTable(
+    raw_entry: *u64,
+    map_type: MapType,
+) !struct { *PageTable, bool } {
+    var created_table = false;
+
+    const next_level_physical_address = blk: {
+        var entry: PageTable.Entry = .{ .raw = raw_entry.* };
+
+        if (entry.present.read()) {
+            if (entry.huge.read()) return error.MappingNotValid;
+
+            break :blk entry.getAddress4kib();
+        }
+        std.debug.assert(entry.raw == 0);
+        created_table = true;
+
+        const backing_range = try kernel.pmm.allocatePage();
+        errdefer comptime unreachable;
+
+        @memset(kernel.vmm.directMapFromPhysicalRange(backing_range).toByteSlice(), 0);
+
+        entry.setAddress4kib(backing_range.address);
+        entry.present.write(true);
+        applyParentMapType(map_type, &entry);
+
+        raw_entry.* = entry.raw;
+
+        break :blk backing_range.address;
+    };
+
+    return .{
+        kernel.vmm
+            .directMapFromPhysical(next_level_physical_address)
+            .toPtr(*PageTable),
+        created_table,
+    };
+}
+
+fn setEntry(
+    page_table: *PageTable,
+    index: usize,
+    physical_address: core.PhysicalAddress,
+    map_type: MapType,
+    comptime page_type: enum { small, medium, large },
+) error{AlreadyMapped}!void {
+    var entry = PageTable.Entry{ .raw = page_table.entries[index] };
+
+    if (entry.present.read()) return error.AlreadyMapped;
+
+    switch (page_type) {
+        .small => entry.setAddress4kib(physical_address),
+        .medium => {
+            entry.huge.write(true);
+            entry.setAddress2mib(physical_address);
+        },
+        .large => {
+            entry.huge.write(true);
+            entry.setAddress1gib(physical_address);
+        },
+    }
+
+    applyMapType(map_type, &entry);
+
+    entry.present.write(true);
+
+    page_table.entries[index] = entry.raw;
+}
+
 pub const all_page_sizes: []const core.Size = &.{
     small_page_size,
     medium_page_size,
@@ -34,6 +127,138 @@ pub const page_table_alignment = PageTable.small_page_size;
 pub const page_table_size = PageTable.small_page_size;
 
 pub const init = struct {
+    /// Maps the `virtual_range` to the `physical_range` with mapping type given by `map_type`.
+    ///
+    /// Caller must ensure:
+    ///  - the virtual range address and size are aligned to the standard page size
+    ///  - the physical range address and size are aligned to the standard page size
+    ///  - the virtual range size is equal to the physical range size
+    ///  - the virtual range is not already mapped
+    ///
+    /// This function:
+    ///  - uses all page sizes available to the architecture
+    ///  - does not flush the TLB
+    ///  - does not rollback on error
+    pub fn mapToPhysicalRangeAllPageSizes(
+        level4_table: *PageTable,
+        virtual_range: core.VirtualRange,
+        physical_range: core.PhysicalRange,
+        map_type: MapType,
+    ) !void {
+        std.debug.assert(virtual_range.address.isAligned(PageTable.small_page_size));
+        std.debug.assert(virtual_range.size.isAligned(PageTable.small_page_size));
+        std.debug.assert(physical_range.address.isAligned(PageTable.small_page_size));
+        std.debug.assert(physical_range.size.isAligned(PageTable.small_page_size));
+        std.debug.assert(virtual_range.size.equal(physical_range.size));
+
+        const supports_1gib = x64.info.cpu_id.gbyte_pages;
+
+        var current_virtual_address = virtual_range.address;
+        const last_virtual_address = virtual_range.last();
+        var current_physical_address = physical_range.address;
+        var size_remaining = virtual_range.size;
+
+        const last_virtual_address_p4_index = PageTable.p4Index(last_virtual_address);
+        const last_virtual_address_p3_index = PageTable.p3Index(last_virtual_address);
+        const last_virtual_address_p2_index = PageTable.p2Index(last_virtual_address);
+
+        var level4_index = PageTable.p4Index(current_virtual_address);
+
+        while (level4_index <= last_virtual_address_p4_index) : (level4_index += 1) {
+            const level3_table, _ = try ensureNextTable(
+                &level4_table.entries[level4_index],
+                map_type,
+            );
+
+            var level3_index = PageTable.p3Index(current_virtual_address);
+            const last_level3_index = if (last_virtual_address_p4_index == level4_index)
+                PageTable.p3Index(last_virtual_address)
+            else
+                PageTable.number_of_entries - 1;
+
+            while (level3_index <= last_level3_index) : (level3_index += 1) {
+                if (supports_1gib and
+                    size_remaining.greaterThanOrEqual(PageTable.large_page_size) and
+                    current_virtual_address.isAligned(PageTable.large_page_size) and
+                    current_physical_address.isAligned(PageTable.large_page_size))
+                {
+
+                    // large 1 GiB page
+                    try setEntry(
+                        level3_table,
+                        level3_index,
+                        current_physical_address,
+                        map_type,
+                        .large,
+                    );
+
+                    current_virtual_address.moveForwardInPlace(PageTable.large_page_size);
+                    current_physical_address.moveForwardInPlace(PageTable.large_page_size);
+                    size_remaining.subtractInPlace(PageTable.large_page_size);
+                    continue;
+                }
+
+                const level2_table, _ = try ensureNextTable(
+                    &level3_table.entries[level3_index],
+                    map_type,
+                );
+
+                var level2_index = PageTable.p2Index(current_virtual_address);
+                const last_level2_index = if (last_virtual_address_p3_index == level3_index)
+                    PageTable.p2Index(last_virtual_address)
+                else
+                    PageTable.number_of_entries - 1;
+
+                while (level2_index <= last_level2_index) : (level2_index += 1) {
+                    if (size_remaining.greaterThanOrEqual(PageTable.medium_page_size) and
+                        current_virtual_address.isAligned(PageTable.medium_page_size) and
+                        current_physical_address.isAligned(PageTable.medium_page_size))
+                    {
+                        // large 2 MiB page
+                        try setEntry(
+                            level2_table,
+                            level2_index,
+                            current_physical_address,
+                            map_type,
+                            .medium,
+                        );
+
+                        current_virtual_address.moveForwardInPlace(PageTable.medium_page_size);
+                        current_physical_address.moveForwardInPlace(PageTable.medium_page_size);
+                        size_remaining.subtractInPlace(PageTable.medium_page_size);
+                        continue;
+                    }
+
+                    const level1_table, _ = try ensureNextTable(
+                        &level2_table.entries[level2_index],
+                        map_type,
+                    );
+
+                    var level1_index = PageTable.p1Index(current_virtual_address);
+                    const last_level1_index = if (last_virtual_address_p2_index == level2_index)
+                        PageTable.p1Index(last_virtual_address)
+                    else
+                        PageTable.number_of_entries - 1;
+
+                    while (level1_index <= last_level1_index) : (level1_index += 1) {
+                        try setEntry(
+                            level1_table,
+                            level1_index,
+                            current_physical_address,
+                            map_type,
+                            .small,
+                        );
+
+                        current_virtual_address.moveForwardInPlace(PageTable.small_page_size);
+                        current_physical_address.moveForwardInPlace(PageTable.small_page_size);
+                        size_remaining.subtractInPlace(PageTable.small_page_size);
+                    }
+                }
+            }
+        }
+    }
+
+    const init_log = kernel.log.scoped(.init_x64_paging);
 };
 
 const small_page_size = PageTable.small_page_size;
@@ -46,3 +271,4 @@ const kernel = @import("kernel");
 const x64 = @import("x64.zig");
 const lib_x64 = @import("x64");
 const PageTable = lib_x64.PageTable;
+const MapType = kernel.vmm.MapType;
