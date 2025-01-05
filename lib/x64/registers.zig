955431490c77e635d2e4df66965b2ea0f6c49c61
Lee Cannon <leecannon@leecannon.xyz>
2025-01-05 00:19:47 +0000

    lib/x64/registers: add debug registers

--- lib/x64/registers.zig
+++ lib/x64/registers.zig
@@ -303,6 +303,211 @@ pub const EFER = packed struct(u64) {
     const msr = MSR(u64, 0xC0000080);
 };
 
+pub const DR0 = DebugAddressRegister(.DR0);
+pub const DR1 = DebugAddressRegister(.DR1);
+pub const DR2 = DebugAddressRegister(.DR2);
+pub const DR3 = DebugAddressRegister(.DR3);
+
+/// Debug-Status Register (DR6)
+///
+/// Debug status is loaded into DR6 when an enabled debug condition is encountered that causes a #DB exception.
+///
+/// `breakpoint_register_access_detected`, `single_step`, and `task_switch` are not cleared by the processor and must be
+/// cleared by software after the contents have been read.
+pub const DR6 = packed struct(u64) {
+    breakpoint_0: bool,
+    breakpoint_1: bool,
+    breakpoint_2: bool,
+    breakpoint_3: bool,
+
+    _reserved4_10: u7 = std.math.maxInt(u7),
+
+    /// The processor set this to `false` if #DB was generated due to a bus lock.
+    ///
+    /// Other sources of #DB do not modify this bit.
+    bus_lock_detected: bool = true,
+
+    _reserved_12: u1 = 0,
+
+    /// The processor sets this bit to 1 if software accesses any debug register (DR0–DR7) while the general-detect
+    /// condition is enabled (`DR7.general_detect = true`).
+    breakpoint_register_access_detected: bool,
+
+    /// The processor sets this bit to 1 if the #DB exception occurs as a result of single-step mode
+    /// (`RFlags.trap = true`).
+    ///
+    /// Single-step mode has the highest-priority among debug exceptions.
+    ///
+    /// Other status bits within the DR6 register can be set by the processor along with the BS bit.
+    single_step: bool,
+
+    /// The processor sets this bit to 1 if the #DB exception occurred as a result of task switch to a task with a
+    /// TSS T-bit set to 1.
+    task_switch: bool,
+
+    _reserved16_31: u16 = std.math.maxInt(u16),
+
+    _reserved32_63: u32 = 0,
+
+    pub fn read() DR6 {
+        return @bitCast(asm ("mov %%dr6, %[value]"
+            : [value] "=r" (-> u64),
+        ));
+    }
+
+    pub fn write(self: DR6) void {
+        asm volatile ("mov %[value], %%dr6"
+            :
+            : [value] "r" (@as(u64, @bitCast(self))),
+        );
+    }
+};
+
+/// Debug-Control Register (DR7)
+///
+/// DR7 is used to establish the breakpoint conditions for the address-breakpoint registers (DR0–DR3) and to enable
+/// debug exceptions for each address-breakpoint register individually.
+///
+/// DR7 is also used to enable the general detect breakpoint condition.
+pub const DR7 = packed struct(u64) {
+    /// When `true` debug exceptions occur when the corresponding address-breakpoint register (DR0) detects a breakpoint
+    /// condition while executing the current task.
+    ///
+    /// Cleared to `false` by the processor when a hardware task-switch occurs.
+    local_exact_breakpoint_0: bool,
+
+    /// When `true` debug exceptions occur when the corresponding address-breakpoint register (DR0) detects a breakpoint
+    /// condition while executing any task.
+    ///
+    /// These bits are never cleared to `false` by the processor.
+    global_exact_breakpoint_0: bool,
+
+    /// When `true` debug exceptions occur when the corresponding address-breakpoint register (DR1) detects a breakpoint
+    /// condition while executing the current task.
+    ///
+    /// Cleared to `false` by the processor when a hardware task-switch occurs.
+    local_exact_breakpoint_1: bool,
+
+    /// When `true` debug exceptions occur when the corresponding address-breakpoint register (DR1) detects a breakpoint
+    /// condition while executing any task.
+    ///
+    /// These bits are never cleared to `false` by the processor.
+    global_exact_breakpoint_1: bool,
+
+    /// When `true` debug exceptions occur when the corresponding address-breakpoint register (DR2) detects a breakpoint
+    /// condition while executing the current task.
+    ///
+    /// Cleared to `false` by the processor when a hardware task-switch occurs.
+    local_exact_breakpoint_2: bool,
+
+    /// When `true` debug exceptions occur when the corresponding address-breakpoint register (DR2) detects a breakpoint
+    /// condition while executing any task.
+    ///
+    /// These bits are never cleared to `false` by the processor.
+    global_exact_breakpoint_2: bool,
+
+    /// When `true` debug exceptions occur when the corresponding address-breakpoint register (DR3) detects a breakpoint
+    /// condition while executing the current task.
+    ///
+    /// Cleared to `false` by the processor when a hardware task-switch occurs.
+    local_exact_breakpoint_3: bool,
+
+    /// When `true` debug exceptions occur when the corresponding address-breakpoint register (DR3) detects a breakpoint
+    /// condition while executing any task.
+    ///
+    /// These bits are never cleared to `false` by the processor.
+    global_exact_breakpoint_3: bool,
+
+    /// This bit is ignored by implementations of the AMD64 architecture.
+    local_exact_breakpoint: bool,
+
+    /// This bit is ignored by implementations of the AMD64 architecture.
+    global_exact_breakpoint: bool,
+
+    _reserved10: u1 = 1,
+
+    _reserved11_12: u2 = 0,
+
+    /// Software sets this to `true` to cause a debug exception to occur when an attempt is made to execute a MOV DRn
+    /// instruction to any debug register (DR0–DR7).
+    ///
+    /// This bit is set to `false` by the processor when the #DB handler is entered, allowing the handler to read and
+    /// write the DRn registers.
+    ///
+    /// The #DB exception occurs before executing the instruction, and `DR6.breakpoint_register_access_detected` is set
+    /// to `true` by the processor.
+    ///
+    /// Software debuggers can use this bit to prevent the currently-executing program from interfering with the debug
+    /// operation.
+    general_detect: bool,
+
+    _reserved14_15: u2 = 0,
+
+    /// Control the breakpoint conditions used by the DR0 register.
+    type_breakpoint_0: BreakpointType,
+    length_breakpoint_0: Length,
+
+    /// Control the breakpoint conditions used by the DR1 register.
+    type_breakpoint_1: BreakpointType,
+    length_breakpoint_1: Length,
+
+    /// Control the breakpoint conditions used by the DR2 register.
+    type_breakpoint_2: BreakpointType,
+    length_breakpoint_2: Length,
+
+    /// Control the breakpoint conditions used by the DR3 register.
+    type_breakpoint_3: BreakpointType,
+    length_breakpoint_3: Length,
+
+    _reserved_32_63: u32 = 0,
+
+    pub const BreakpointType = enum(u2) {
+        /// Only on instruction execution.
+        ///
+        /// The `length` field for the register using this type must be set to `.byte`.
+        /// Setting to any other value produces undefined results.
+        instruction_execution = 0b00,
+
+        /// Only on data write.
+        data_write = 0b01,
+
+        /// Effect depends on the value of `CR4[DE]` as follows:
+        /// - `CR4[DE] = 0` - Condition is undefined.
+        /// - `CR4[DE] = 1` - Only on I/O read or I/O write.
+        io_read_write = 0b10,
+
+        /// Only on data read or data write.
+        read_write = 0b11,
+    };
+
+    pub const Length = enum(u2) {
+        /// 1 byte.
+        byte = 0b00,
+
+        /// 2 bytes, address must be 2 byte aligned.
+        word = 0b01,
+
+        /// 4 bytes, address must be 4 byte aligned.
+        dword = 0b10,
+
+        /// 8 bytes, address must be 8 byte aligned.
+        qword = 0b11,
+    };
+
+    pub fn read() DR7 {
+        return @bitCast(asm ("mov %%dr7, %[value]"
+            : [value] "=r" (-> u64),
+        ));
+    }
+
+    pub fn write(self: DR7) void {
+        asm volatile ("mov %[value], %%dr7"
+            :
+            : [value] "r" (@as(u64, @bitCast(self))),
+        );
+    }
+};
+
 /// Processors based on Nehalem microarchitecture provide an auxiliary TSC register, IA32_TSC_AUX that is designed to
 /// be used in conjunction with IA32_TSC.
 ///
@@ -372,6 +577,48 @@ pub fn MSR(comptime T: type, comptime register: u32) type {
 
         pub inline fn write(value: T) void {
             writeMSR(T, register, value);
+        }
+    };
+}
+
+fn DebugAddressRegister(comptime register: enum { DR0, DR1, DR2, DR3 }) type {
+    return struct {
+        pub fn read() core.VirtualAddress {
+            return switch (register) {
+                .DR0 => .fromInt(asm ("mov %%dr0, %[value]"
+                    : [value] "=r" (-> u64),
+                )),
+                .DR1 => .fromInt(asm ("mov %%dr1, %[value]"
+                    : [value] "=r" (-> u64),
+                )),
+                .DR2 => .fromInt(asm ("mov %%dr2, %[value]"
+                    : [value] "=r" (-> u64),
+                )),
+                .DR3 => .fromInt(asm ("mov %%dr3, %[value]"
+                    : [value] "=r" (-> u64),
+                )),
+            };
+        }
+
+        pub fn write(address: core.VirtualAddress) void {
+            switch (register) {
+                .DR0 => asm volatile ("mov %[address], %%dr0"
+                    :
+                    : [address] "r" (address.value),
+                ),
+                .DR1 => asm volatile ("mov %[address], %%dr1"
+                    :
+                    : [address] "r" (address.value),
+                ),
+                .DR2 => asm volatile ("mov %[address], %%dr2"
+                    :
+                    : [address] "r" (address.value),
+                ),
+                .DR3 => asm volatile ("mov %[address], %%dr3"
+                    :
+                    : [address] "r" (address.value),
+                ),
+            }
         }
     };
 }
