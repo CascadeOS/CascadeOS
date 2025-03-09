aad6fda9e46697c84e5f602c023461dc8d73623f
Lee Cannon <leecannon@leecannon.xyz>
2025-03-09 21:11:15 +0000

    kernel/init/devicetree: update to use new upstream api

--- kernel/init/devicetree.zig
+++ kernel/init/devicetree.zig
@@ -28,6 +28,10 @@ pub fn tryGetSerialOutput() ?uart.Uart {
             log.warn("baud divisor too large", .{});
             return null;
         },
+        error.SizeNotMultiple => {
+            log.warn("the regs property size is not a multiple of the address-cells + size-cells", .{});
+            return null;
+        },
     } orelse return null;
 
     return output_uart;
@@ -38,55 +42,43 @@ fn tryGetSerialOutputInner() GetSerialOutputError!?uart.Uart {
 
     if (try getSerialOutputFromChosenNode(dt)) |output_uart| return output_uart;
 
-    // TODO: doing a iteration per supported UART is not ideal
-    for (compatible_lookup.keys()) |key| {
-        if (try dt.findNodeWithCompatible(key)) |node_with_name| {
-            const func = compatible_lookup.get(key).?;
-            if (try func(dt, node_with_name.node)) |output_uart| return output_uart;
-        }
+    var iter = dt.compatibleMatchIterator({}, matchFunction);
+
+    while (try iter.next(dt)) |compatible_match| {
+        const func = compatible_lookup.get(compatible_match.compatible).?;
+        if (try func(dt, compatible_match.node.node)) |output_uart| return output_uart;
     }
 
     return null;
 }
 
 fn getSerialOutputFromChosenNode(dt: DeviceTree) GetSerialOutputError!?uart.Uart {
-    const chosen_node = blk: {
-        var iter = try DeviceTree.Node.root.iterateSubnodes(
-            dt,
-            .direct_children,
-            .{ .name = "chosen" },
-        );
-        break :blk (try iter.next()) orelse return null;
-    };
+    const chosen_node = (try DeviceTree.Node.root.firstMatchingSubnode(
+        dt,
+        .direct_children,
+        .{ .name = "chosen" },
+    )) orelse return null;
 
-    const stdout_path_property = blk: {
-        var iter = try chosen_node.node.propertyIterator(dt, .{ .name = "stdout-path" });
-        break :blk (try iter.next()) orelse return null;
-    };
+    const stdout_path_property = (try chosen_node.node.firstMatchingProperty(
+        dt,
+        .{ .name = "stdout-path" },
+    )) orelse return null;
 
     const stdout_path = stdout_path_property.value.toString();
 
     const node = (dt.nodeFromPath(stdout_path) catch |err| switch (err) {
         error.BadOffset, error.Truncated => |e| return e,
-        error.NonExistentAlias => {
-            log.warn("the chosen nodes stdout-path property refers to a non-existent alias", .{});
-            return null;
-        },
         error.BadPath => {
             log.warn("the chosen nodes stdout-path property is not a valid path", .{});
             return null;
         },
     }) orelse return null;
 
-    const compatible_property = blk: {
-        var iter = try node.node.propertyIterator(dt, .{ .name = "compatible" });
-        break :blk (try iter.next()) orelse return null;
-    };
+    var compatible_iter = try node.node.compatibleIterator(dt);
 
-    var compatible_list = compatible_property.value.stringListIterator();
-    while (try compatible_list.next()) |compatible| {
+    while (try compatible_iter.next()) |compatible| {
         if (compatible_lookup.get(compatible)) |getSerialOutputFn| {
-            return getSerialOutputFn(dt, node.node);
+            return try getSerialOutputFn(dt, node.node);
         }
     }
 
@@ -95,22 +87,32 @@ fn getSerialOutputFromChosenNode(dt: DeviceTree) GetSerialOutputError!?uart.Uart
 
 fn getSerialOutputFromNS16550a(dt: DeviceTree, node: DeviceTree.Node) GetSerialOutputError!?uart.Uart {
     const clock_frequency = blk: {
-        var iter = try node.propertyIterator(dt, .{ .name = "clock-frequency" });
-        const clock_frequency_property = (try iter.next()) orelse {
+        const clock_frequency_property = (try node.firstMatchingProperty(
+            dt,
+            .{ .name = "clock-frequency" },
+        )) orelse {
             log.warn("no clock-frequency property found for ns16550a", .{});
             return null;
         };
         break :blk clock_frequency_property.value.toU32();
     };
     const address = blk: {
-        var iter = try node.propertyIterator(dt, .{ .name = "reg" });
-        const reg_property = (try iter.next()) orelse {
+        const reg_property = (try node.firstMatchingProperty(
+            dt,
+            .{ .name = "reg" },
+        )) orelse {
             log.warn("no reg property found for ns16550a", .{});
             return null;
         };
-        // FIXME: remove this once zig-devicetree has support for integer property iteration
-        const ptr: *align(1) const u64 = @ptrCast(reg_property.value._raw);
-        break :blk std.mem.bigToNative(u64, ptr.*);
+
+        // FIXME: rather than assume address-cells and size-cells are both two, we should actually look at the parent
+        var reg_iter = try reg_property.value.regIterator(2, 2);
+
+        const reg = reg_iter.next() orelse {
+            log.warn("no reg property found for ns16550a", .{});
+            return null;
+        };
+        break :blk reg.address;
     };
 
     return .{
@@ -127,37 +129,54 @@ fn getSerialOutputFromNS16550a(dt: DeviceTree, node: DeviceTree.Node) GetSerialO
 }
 
 fn getSerialOutputFromPL011(dt: DeviceTree, node: DeviceTree.Node) GetSerialOutputError!?uart.Uart {
-    const address = blk: {
-        var iter = try node.propertyIterator(dt, .{ .name = "reg" });
-        const reg_property = (try iter.next()) orelse {
-            log.warn("no reg property found for pl011", .{});
+    const clock_frequency = clock_frequency: {
+        const clocks_property = (try node.firstMatchingProperty(
+            dt,
+            .{ .name = "clocks" },
+        )) orelse {
+            log.warn("no clocks property found for pl011", .{});
             return null;
         };
-        // FIXME: remove this once zig-devicetree has support for integer property iteration
-        const ptr: *align(1) const u64 = @ptrCast(reg_property.value._raw);
-        break :blk std.mem.bigToNative(u64, ptr.*);
-    };
-    const clock_frequency = clock_frequency: {
-        const clocks_property = blk: {
-            var iter = try node.propertyIterator(dt, .{ .name = "clocks" });
-            break :blk (try iter.next()) orelse {
-                log.warn("no clocks property found for pl011", .{});
-                return null;
-            };
-        };
+
         // there are multiple clocks, but the first one happens to be the one we want
-        const clock_phandle = clocks_property.value.toPHandle();
+        var clocks_iter = try clocks_property.value.pHandleListIterator();
+        const clock_phandle = clocks_iter.next() orelse {
+            log.warn("no clocks phandle found for pl011", .{});
+            return null;
+        };
+
         const clock_node = (try clock_phandle.node(dt)) orelse {
             log.warn("no clock node found for pl011", .{});
             return null;
         };
 
-        var iter = try clock_node.node.propertyIterator(dt, .{ .name = "clock-frequency" });
-        const clock_frequency_property = (try iter.next()) orelse {
-            log.warn("no clock-frequency property found for ns16550a", .{});
+        const clock_frequency_property = (try clock_node.node.firstMatchingProperty(
+            dt,
+            .{ .name = "clock-frequency" },
+        )) orelse {
+            log.warn("no clock-frequency property found for pl011", .{});
             return null;
         };
+
         break :clock_frequency clock_frequency_property.value.toU32();
+    };
+    const address = blk: {
+        const reg_property = (try node.firstMatchingProperty(
+            dt,
+            .{ .name = "reg" },
+        )) orelse {
+            log.warn("no reg property found for pl011", .{});
+            return null;
+        };
+
+        // FIXME: rather than assume address-cells and size-cells are both two, we should actually look at the parent
+        var reg_iter = try reg_property.value.regIterator(2, 2);
+
+        const reg = reg_iter.next() orelse {
+            log.warn("no reg property found for pl011", .{});
+            return null;
+        };
+        break :blk reg.address;
     };
 
     return .{
@@ -173,12 +192,18 @@ fn getSerialOutputFromPL011(dt: DeviceTree, node: DeviceTree.Node) GetSerialOutp
     };
 }
 
+fn matchFunction(_: void, compatible: [:0]const u8) bool {
+    return compatible_lookup.get(compatible) != null;
+}
+
 const compatible_lookup = std.StaticStringMap(GetSerialOutputFn).initComptime(.{
     .{ "ns16550a", getSerialOutputFromNS16550a },
     .{ "arm,pl011", getSerialOutputFromPL011 },
 });
 
-const GetSerialOutputError = DeviceTree.IteratorError || uart.Baud.DivisorError;
+const GetSerialOutputError = DeviceTree.IteratorError ||
+    DeviceTree.Property.Value.ListIteratorError ||
+    uart.Baud.DivisorError;
 const GetSerialOutputFn = *const fn (dt: DeviceTree, node: DeviceTree.Node) GetSerialOutputError!?uart.Uart;
 
 const std = @import("std");
