b1ba86b035c8553c1e2f311804a336e46b4b4f64
Lee Cannon <leecannon@leecannon.xyz>
2025-05-22 16:48:08 +0100

    kernel/sync/TicketSpinLock: drop usage of `std.atomic.Value`

--- kernel/sync/TicketSpinLock.zig
+++ kernel/sync/TicketSpinLock.zig
@@ -4,8 +4,8 @@
 //! A simple spinlock implementation using tickets to ensure fairness.
 const TicketSpinLock = @This();
 
-current: std.atomic.Value(u32) = .init(0),
-ticket: std.atomic.Value(u32) = .init(0),
+current: u32 = 0,
+ticket: u32 = 0,
 holding_executor: kernel.Executor.Id = .none,
 
 /// Locks the spinlock.
@@ -15,16 +15,17 @@ pub fn lock(self: *TicketSpinLock, current_task: *kernel.Task) void {
     const executor = current_task.state.running;
     std.debug.assert(!self.isLockedByCurrent(current_task)); // recursive locks are not supported
 
-    const ticket = self.ticket.fetchAdd(1, .monotonic);
-    if (self.current.load(.acquire) != ticket) {
+    const ticket = @atomicRmw(u32, &self.ticket, .Add, 1, .monotonic);
+
+    if (@atomicLoad(u32, &self.current, .acquire) != ticket) {
         @branchHint(.unlikely);
 
         while (true) {
             kernel.arch.spinLoopHint();
-            if (self.current.load(.monotonic) == ticket) break;
+            if (@atomicLoad(u32, &self.current, .monotonic) == ticket) break;
         }
 
-        _ = self.current.load(.acquire);
+        _ = @atomicLoad(u32, &self.current, .acquire);
     }
 
     self.holding_executor = executor.id;
@@ -49,12 +50,12 @@ pub fn unlock(self: *TicketSpinLock, current_task: *kernel.Task) void {
 /// Performs no checks and is unsafe, prefer `unlock` instead.
 pub inline fn unsafeUnlock(self: *TicketSpinLock) void {
     self.holding_executor = .none;
-    self.current.store(self.current.raw +% 1, .release);
+    @atomicStore(u32, &self.current, self.current +% 1, .release);
 }
 
 /// Poison the spinlock, this will cause any future attempts to lock the spinlock to deadlock.
 pub fn poison(self: *TicketSpinLock) void {
-    _ = self.current.fetchSub(1, .release);
+    _ = @atomicRmw(u32, &self.current, .Sub, 1, .release);
 }
 
 /// Returns true if the spinlock is locked by the current executor.
