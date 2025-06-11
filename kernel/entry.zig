97b131c9ad6ab78d7281c5ff76eecf82ece0e4d4
Lee Cannon <leecannon@leecannon.xyz>
2025-06-11 21:19:10 +0100

    kernel/entry: specify whether interrupt disable is decremented on entry functions

--- kernel/entry.zig
+++ kernel/entry.zig
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: Lee Cannon <leecannon@leecannon.xyz>
 
+/// Executed upon per executor periodic interrupt.
+///
+/// Interrupt disable is _not_ decremented before calling this function.
 pub fn onPerExecutorPeriodic(current_task: *kernel.Task) void {
     // TODO: do more than just preempt on every interrupt
 
     kernel.scheduler.maybePreempt(current_task);
 }
 
+/// Executed upon page fault.
+///
+/// Interrupt disable _is_ decremented before calling this function.
 pub fn onPageFault(current_task: *kernel.Task, page_fault_details: kernel.mem.PageFaultDetails) void {
     switch (page_fault_details.source) {
         .kernel => kernel.mem.onKernelPageFault(current_task, page_fault_details),
@@ -14,6 +20,9 @@ pub fn onPageFault(current_task: *kernel.Task, page_fault_details: kernel.mem.Pa
     }
 }
 
+/// Executed upon cross-executor flush request.
+///
+/// Interrupt disable is _not_ decremented before calling this function.
 pub fn onFlushRequest(current_task: *kernel.Task) void {
     kernel.mem.FlushRequest.processFlushRequests(current_task);
 }
