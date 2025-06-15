08c287c73c627b2fdab5be3845fbc892dacc0dcc
Lee Cannon <leecannon@leecannon.xyz>
2025-06-15 01:49:38 +0100

    kernel/riscv/init: detect and use the SBI debug console for init output
    prefer the sbi debug console over looking in ACPI tables

--- kernel/arch/riscv/init.zig
+++ kernel/arch/riscv/init.zig
@@ -23,6 +23,11 @@ pub fn getStandardWallclockStartTime() kernel.time.wallclock.Tick {
 /// This function can return an architecture specific output if it is available and if not is expected to call into
 /// `kernel.init.Output.tryGetSerialOutputFromGenericSources`.
 pub fn tryGetSerialOutput() ?kernel.init.Output {
+    if (SBIDebugConsole.detect()) {
+        log.debug("using sbi debug console for serial output", .{});
+        return SBIDebugConsole.output;
+    }
+
     return kernel.init.Output.tryGetSerialOutputFromGenericSources();
 }
 
@@ -41,8 +46,47 @@ pub fn loadExecutor(executor: *kernel.Executor) void {
     lib_riscv.registers.SupervisorScratch.write(@intFromPtr(executor));
 }
 
+const SBIDebugConsole = struct {
+    fn detect() bool {
+        return sbi.debug_console.available();
+    }
+
+    const output: kernel.init.Output = .{
+        .writeFn = struct {
+            fn writeFn(_: *anyopaque, str: []const u8) void {
+                // TODO: figure out how to get `sbi.debug_console.write` to work
+                //       as `sbi.debug_console.writeByte` is inefficient
+                for (0..str.len) |i| {
+                    const byte = str[i];
+
+                    if (byte == '\n') {
+                        @branchHint(.unlikely);
+
+                        const newline_first_or_only = str.len == 1 or i == 0;
+
+                        if (newline_first_or_only or str[i - 1] != '\r') {
+                            @branchHint(.likely);
+                            sbi.debug_console.writeByte('\r') catch return;
+                        }
+                    }
+
+                    sbi.debug_console.writeByte(byte) catch return;
+                }
+            }
+        }.writeFn,
+        .remapFn = struct {
+            fn remapFn(_: *anyopaque, _: *kernel.Task) !void {
+                return;
+            }
+        }.remapFn,
+        .context = undefined,
+    };
+};
+
 const std = @import("std");
 const core = @import("core");
 const kernel = @import("kernel");
 const riscv = @import("riscv.zig");
 const lib_riscv = @import("riscv");
+const log = kernel.debug.log.scoped(.init_riscv);
+const sbi = @import("sbi");
