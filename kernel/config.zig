0c501901bc9869bc281ec1316849f4a506420bd5
Lee Cannon <leecannon@leecannon.xyz>
2025-06-28 23:35:14 +0100

    kernel/config: increase name lengths

--- kernel/config.zig
+++ kernel/config.zig
@@ -15,10 +15,10 @@ pub const maximum_number_of_time_sources = 8;
 
 pub const maximum_number_of_executors = 64;
 
-pub const task_name_length = 32;
-pub const resource_arena_name_length = 32;
+pub const task_name_length = 64;
+pub const resource_arena_name_length = 64;
 pub const address_space_name_length = resource_arena_name_length - "_address_arena".len;
-pub const cache_name_length = 32;
+pub const cache_name_length = 64;
 
 pub const per_executor_interrupt_period = core.Duration.from(5, .millisecond);
 
