ab48660ba0e5c20da906ff41a8c664f2a1ddf64a
Lee Cannon <leecannon@leecannon.xyz>
2026-01-27 18:18:07 +0000

    user/hello_world: create a simple userspace hello world program
    It's so simple it doesn't even print hello world.

---
+++ user/hello_world/hello_world.zig
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: LicenseRef-NON-AI-MIT
+// SPDX-FileCopyrightText: Lee Cannon <leecannon@leecannon.xyz>
+
+const std = @import("std");
+
+const cascade = @import("cascade");
+
+pub export const _start = cascade._cascade_start;
+
+pub fn main() void {
+    // TODO: actually print "hello world"...
+}
--- user/listing.zig
+++ user/listing.zig
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: LicenseRef-NON-AI-CC0-1.0
 // SPDX-FileCopyrightText: Lee Cannon <leecannon@leecannon.xyz>
 
-pub const applications: []const ApplicationDescription = &.{};
+pub const applications: []const ApplicationDescription = &.{
+    .{
+        .name = "hello_world",
+        .dependencies = &.{"cascade"},
+    },
+};
 
 const ApplicationDescription = @import("../build/ApplicationDescription.zig");
