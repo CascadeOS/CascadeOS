6df4b79874d3ce8a480c19566ce8650a4db320fc
Lee Cannon <leecannon@protonmail.com>
2026-06-18 20:10:19 +0100

    kernel/user: split individual syscalls out into their own functions

--- kernel/cascade/user/user.zig
+++ kernel/cascade/user/user.zig
@@ -32,73 +32,76 @@ pub fn onSyscall(
     log.verbose("received syscall: {t}", .{syscall});
 
     switch (syscall) {
-        .thread_exit_current => {
-            const scheduler_handle: cascade.Task.Scheduler.Handle = .get();
-            scheduler_handle.terminate();
-            unreachable;
-        },
+        .thread_exit_current => syscallThreadExitCurrent(),
         .debug_print => {
-            const full_source = (syscall_frame.getUserRange(.two, .one) orelse return 0).toVirtualRange();
-            if (full_source.size.equal(.zero)) return 0;
-
-            const writer = cascade.init.Output.terminal.writer;
-            if (core.is_debug) std.debug.assert(writer.buffer.len != 0); // assumed by below loop to be non-zero
-
-            const full_destination = cascade.KernelVirtualRange.fromSlice(u8, writer.buffer).toVirtualRange();
-            var bytes_copied: core.Size = .zero;
-
-            // TODO: remove usage of `init.Output` as this is intended to be disabled by the time userspace is running...
-            //       and printing logs during page faults that occur during the safe memcpy will deadlock
-            cascade.init.Output.lock.lock();
-            defer {
-                writer.end = 0;
-                cascade.init.Output.lock.unlock();
-            }
-            if (core.is_debug) std.debug.assert(writer.end == 0);
-
-            const process: *const Process = .from(current_task.task);
-            writer.print("{f}: ", .{process}) catch {
+            syscallDebugPrint(current_task, syscall_frame) catch {
                 @branchHint(.cold);
-                return 0;
             };
-
-            var last_copy = false;
-
-            while (!last_copy) {
-                const bytes_to_copy: core.Size = .from(
-                    @min(full_source.size.subtract(bytes_copied).value, writer.buffer.len - writer.end),
-                    .byte,
-                );
-
-                if (!cascade.mem.safe.memcpy(.{
-                    .destination = full_destination.subslice(
-                        .from(writer.end, .byte),
-                        bytes_to_copy,
-                    ),
-                    .source = full_source.subslice(bytes_copied, bytes_to_copy),
-                })) {
-                    @branchHint(.cold);
-                    return 0;
-                }
-
-                bytes_copied.addInPlace(bytes_to_copy);
-                last_copy = bytes_copied.greaterThanOrEqual(full_source.size);
-
-                writer.end += bytes_to_copy.value;
-
-                if (last_copy and writer.buffer[writer.end - 1] != '\n') writer.writeByte('\n') catch {
-                    @branchHint(.cold);
-                    return 0;
-                };
-
-                writer.flush() catch {
-                    @branchHint(.cold);
-                    return 0;
-                };
-            }
-
             return 0;
         },
+    }
+}
+
+fn syscallThreadExitCurrent() noreturn {
+    const scheduler_handle: cascade.Task.Scheduler.Handle = .get();
+    scheduler_handle.terminate();
+    unreachable;
+}
+
+fn syscallDebugPrint(
+    current_task: cascade.Task.Current,
+    syscall_frame: arch.user.SyscallFrame,
+) !void {
+    const full_source = (syscall_frame.getUserRange(.two, .one) orelse return).toVirtualRange();
+    if (full_source.size.equal(.zero)) return;
+
+    const writer = cascade.init.Output.terminal.writer;
+    if (core.is_debug) std.debug.assert(writer.buffer.len != 0); // assumed by below loop to be non-zero
+
+    const full_destination = cascade.KernelVirtualRange.fromSlice(u8, writer.buffer).toVirtualRange();
+    var bytes_copied: core.Size = .zero;
+
+    // TODO: remove usage of `init.Output` as this is intended to be disabled by the time userspace is running...
+    //       and printing logs during page faults that occur during the safe memcpy will deadlock
+    cascade.init.Output.lock.lock();
+    defer {
+        writer.end = 0;
+        cascade.init.Output.lock.unlock();
+    }
+    if (core.is_debug) std.debug.assert(writer.end == 0);
+
+    const process: *const Process = .from(current_task.task);
+    try writer.print("{f}: ", .{process});
+
+    var last_copy = false;
+
+    while (!last_copy) {
+        const bytes_to_copy: core.Size = .from(
+            @min(full_source.size.subtract(bytes_copied).value, writer.buffer.len - writer.end),
+            .byte,
+        );
+
+        if (!cascade.mem.safe.memcpy(.{
+            .destination = full_destination.subslice(
+                .from(writer.end, .byte),
+                bytes_to_copy,
+            ),
+            .source = full_source.subslice(bytes_copied, bytes_to_copy),
+        })) {
+            @branchHint(.cold);
+            return;
+        }
+
+        bytes_copied.addInPlace(bytes_to_copy);
+        last_copy = bytes_copied.greaterThanOrEqual(full_source.size);
+
+        writer.end += bytes_to_copy.value;
+
+        if (last_copy and writer.buffer[writer.end - 1] != '\n') {
+            try writer.writeByte('\n');
+        }
+
+        try writer.flush();
     }
 }
 
