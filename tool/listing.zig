8fe43ef0c91e8efb9f0b131abab8c32c73ef4850
Lee Cannon <leecannon@protonmail.com>
2026-06-21 00:01:12 +0100

    tool/limine_install: force llvm
    We hit an SFrame related linker error with the non-llvm backend

--- tool/listing.zig
+++ tool/listing.zig
@@ -7,6 +7,7 @@ pub const tools: []const ToolDescription = &.{
     .{ .name = "image_builder", .dependencies = &.{ "core", "fs", "uuid" } },
     .{
         .name = "limine_install",
+        .force_llvm = true, // TODO: needed due to https://codeberg.org/ziglang/zig/issues/31272
         .configuration = .{
             .custom = @import("limine_install/custom_configuration.zig").customConfiguration,
         },
